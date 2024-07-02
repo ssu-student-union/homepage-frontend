73056a4a58e6d7b455630f72a64e6330a2cc7187
dvp-tae <th3016@naver.com>
2024-07-02 17:32:40 +0900

    style: boardNavigator onClick 이벤트 수정

--- src/components/Board/BoardNavigator.tsx
+++ src/components/Board/BoardNavigator.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { Button } from "../ui/button";
 
 interface BoardNavigatorProp {
@@ -5,17 +6,25 @@ interface BoardNavigatorProp {
 }
 
 export function BoardNavigator({ subcategory }: BoardNavigatorProp) {
+  const [selectedIndex, setSelectedIndex] = useState(0);
+
   const baseButtonStyle = "h-[37px] px-4 py-2 border rounded-[32px] font-bold";
   const unselectedStyle = `${baseButtonStyle} border-black bg-white text-black hover:bg-primary hover:text-white hover:border-primary`;
   const selectedStyle = `${baseButtonStyle} border-primary bg-primary text-white`;
+
+  const handleClick = (index: number) => {
+    setSelectedIndex(index);
+  };
+
   return (
     <div className="ml-[85px] mt-10 flex gap-2">
       {subcategory.map((title, index) => {
-        const isSelected = index === 0;
+        const isSelected = index === selectedIndex;
         return (
           <Button
             key={title}
             className={isSelected ? selectedStyle : unselectedStyle}
+            onClick={() => handleClick(index)}
           >
             {title}
           </Button>
