900b09abd32658709eb7fdd94bfe23928c3f7407
dvp-tae <th3016@naver.com>
2024-07-30 15:12:26 +0900

    feat: add petition page reactive layout

--- src/components/Search/Search.tsx
+++ src/components/Search/Search.tsx
@@ -9,7 +9,7 @@ export function Search() {
 
   useEffect(() => {
     if (inputRef.current) {
-      if (width > 1439 || (width > 390 && width <= 1079)) {
+      if (width > 1439 || (width > 719 && width <= 1079)) {
         inputRef.current.placeholder = "원하시는 키워드를 입력하세요";
       } else {
         inputRef.current.placeholder = "키워드 입력";
@@ -23,10 +23,10 @@ export function Search() {
         ref={inputRef}
         type="text"
         className="
-        border-[#959595] placeholder:text-[#B6B6B6] text-base  xxl:w-[488px] xxl:h-[58px] xl:w-[488px] xl:h-[58px] lg:w-[254px] lg:h-[58px] md:w-[488px] md:h-[58px] sm:w-[488px] sm:h-[58px] xs:w-[211px] xs:h-[46px]"
+        border-[#959595] placeholder:text-[#B6B6B6] text-base  xxl:w-[488px] xxl:h-[58px] xl:w-[488px] xl:h-[58px] lg:w-[254px] lg:h-[58px] md:w-[488px] md:h-[58px] sm:w-[211px] sm:h-[46px] xs:w-[211px] xs:h-[46px]"
         placeholder="원하시는 키워드를 입력하세요"
       />
-      <Button className="text-base xxl:w-[94px] xxl:h-[58px] xl:w-[94px] xl:h-[58px] lg:w-[94px] lg:h-[58px] md:w-[94px] md:h-[58px] sm:w-[94px] sm:h-[58px] xs:w-[77px] xs:h-[46px]">
+      <Button className="text-base xxl:w-[94px] xxl:h-[58px] xl:w-[94px] xl:h-[58px] lg:w-[94px] lg:h-[58px] md:w-[94px] md:h-[58px] sm:w-[77px] sm:h-[46px] xs:w-[77px] xs:h-[46px]">
         검색
       </Button>
     </div>
--- src/pages/petition-notice/containers/PetitionPostSection.tsx
+++ src/pages/petition-notice/containers/PetitionPostSection.tsx
@@ -3,7 +3,6 @@ import { Petition } from "@/types";
 import { Partnership } from "@/types";
 import { useState, useEffect } from "react";
 import { useNavigate, useSearchParams } from "react-router-dom";
-
 import { WriteButton } from "@/components/Buttons/BoardActionButtons";
 import { Search } from "@/components/Search/Search";
 import { PetitionPostContent } from "./PetitionPostContent";
@@ -36,23 +35,30 @@ export function PetitionPostSection() {
         selectedSubcategory={selectedSubcategory}
         onSubcategorySelect={handleSubcategorySelect}
       />
-      <div className="mt-11 relative">
-        <PetitionPostContent />
-        <PetitionPostContent />
-        <PetitionPostContent />
-        <PetitionPostContent />
-        <PetitionPostContent />
-        <PetitionPostContent />
-        <PetitionPostContent />
-        <PetitionPostContent />
-        <div className="absolute bottom-[-70px] right-0">
-          <WriteButton onClick={handleWriteBtnClick} />
+      <div className="mt-11">
+        <div className="relative">
+          <PetitionPostContent />
+          <PetitionPostContent />
+          <PetitionPostContent />
+          <PetitionPostContent />
+          <PetitionPostContent />
+          <PetitionPostContent />
+          <PetitionPostContent />
+          <PetitionPostContent />
+        </div>
+        <div className="flex sm:flex-col-reverse sm:mt-9 xs:flex-col-reverse xs:mt-9 md:justify-between md:mt-8 lg:justify-between lg:mt-8 xl:justify-between xl:mt-8 xxl:justify-between xxl:mt-8">
+          <div className="w-[94px]  "></div>
+          <div className="flex justify-center sm:mt-[17px] xs:mt-[17px]">
+            <div className="w-[253px] h-[34px] border bg-primary text-center text-sm font-light lg:mt-[66px] xl:mt-[66px] xxl:mt-[66px]">
+              pagination components
+            </div>
+          </div>
+          <div className="flex sm:justify-center xs:justify-center justify-end">
+            <WriteButton onClick={handleWriteBtnClick} />
+          </div>
         </div>
       </div>
-      <div className="mt-[92px] text-center">
-        <div>페이지네이션 들어갈 자리</div>
-      </div>
-      <div className="mt-[116px] lg:hidden xl:hidden xxl:hidden flex justify-center">
+      <div className="sm:mt-[17px] xs:mt-[17px] md:mt-[42px] lg:hidden xl:hidden xxl:hidden flex justify-center">
         <Search />
       </div>
     </div>
