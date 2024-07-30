c6371d1357506c67b39500e82aefddd6aa8b76b7
intersoom <sallybig@naver.com>
2024-07-30 16:42:48 +0900

    fix: add select-none to avoid user's highlight

--- src/components/ui/pagination.tsx
+++ src/components/ui/pagination.tsx
@@ -38,7 +38,11 @@ const PaginationItem = React.forwardRef<
   HTMLLIElement,
   React.ComponentProps<"li">
 >(({ className, ...props }, ref) => (
-  <li ref={ref} className={cn("", (className = "cursor-pointer"))} {...props} />
+  <li
+    ref={ref}
+    className={cn("cursor-pointer select-none", className)}
+    {...props}
+  />
 ));
 PaginationItem.displayName = "PaginationItem";
 
