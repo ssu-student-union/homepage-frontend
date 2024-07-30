07e1aac2b8096e732aeba572a027d79edfbd22fc
intersoom <sallybig@naver.com>
2024-07-31 02:31:10 +0900

    feat: add button to MainHeroSection

--- src/pages/main/containers/MainHeroSection.tsx
+++ src/pages/main/containers/MainHeroSection.tsx
@@ -1,11 +1,14 @@
 export function MainHeroSection() {
   return (
-    <div className="h-[100dvh] w-full">
-      <div className="h-full flex justify-center items-center">
+    <div className="h-full w-full z-40 absolute text-primary-foreground">
+      <div className="h-full flex justify-center items-center flex-col gap-[10px]">
         <div className="flex flex-col items-center text-center">
           <div className="font-bold text-xl">제 64대 총학생회</div>
           <h1 className="text-[80px] font-black leading-none">US:SUM</h1>
         </div>
+        <button className="bg-transparent border-[1px] rounded-full border-white h-[46px] w-[173px] flex justify-center items-center">
+          <p className="font-bold">로그인 하러가기</p>
+        </button>
       </div>
     </div>
   );
