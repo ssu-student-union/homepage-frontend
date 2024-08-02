383bf54eb438da91379182993a0c87fc603491b5
haesol822 <dordork@naver.com>
2024-08-02 22:00:03 +0900

    fix: 프리티어 적용

--- src/components/Floating/Floating.tsx
+++ src/components/Floating/Floating.tsx
@@ -1,23 +1,17 @@
-import { linkPaths, iconPaths } from "./const";
+import { linkPaths, iconPaths } from './const';
 
 interface FloatingIconProps {
-  icon: "kakao" | "insta" | "youtube";
+  icon: 'kakao' | 'insta' | 'youtube';
   fill?: string;
   size?: string;
   viewBox: string;
-  path: "kakaoLink" | "instaLink" | "youtubeLink";
+  path: 'kakaoLink' | 'instaLink' | 'youtubeLink';
 }
 
-const FloatingIcon = ({
-  icon,
-  fill = "#ffffff",
-  size = "34px",
-  viewBox,
-  path,
-}:FloatingIconProps) => (
+const FloatingIcon = ({ icon, fill = '#ffffff', size = '34px', viewBox, path }: FloatingIconProps) => (
   <a
     href={linkPaths[path]}
-    className="bg-primary hover:bg-blue-700 flex justify-center items-center w-20 h-20 rounded-[40px] cursor-pointer"
+    className="flex h-20 w-20 cursor-pointer items-center justify-center rounded-[40px] bg-primary hover:bg-blue-700"
   >
     <svg
       xmlns="http://www.w3.org/2000/svg"
@@ -32,27 +26,8 @@ const FloatingIcon = ({
   </a>
 );
 
-export const KakaoFloating = () => (
-  <FloatingIcon
-    icon="kakao"
-    viewBox="0 0 34 32"
-    path="kakaoLink"
-  />
-);
+export const KakaoFloating = () => <FloatingIcon icon="kakao" viewBox="0 0 34 32" path="kakaoLink" />;
 
-export const InstaFloating = () => (
-  <FloatingIcon
-    icon="insta"
-    viewBox="0 0 34 34"
-    path="instaLink"
-  />
-);
+export const InstaFloating = () => <FloatingIcon icon="insta" viewBox="0 0 34 34" path="instaLink" />;
 
-export const YoutubeFloating = () => (
-  <FloatingIcon
-    icon="youtube"
-    viewBox="0 0 42 42"
-    size="42px"
-    path="youtubeLink"
-  />
-);
+export const YoutubeFloating = () => <FloatingIcon icon="youtube" viewBox="0 0 42 42" size="42px" path="youtubeLink" />;
