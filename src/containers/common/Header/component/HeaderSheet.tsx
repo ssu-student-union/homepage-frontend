2c3f5435fe657bd0f6716f3a131e03bde89f4b83
sejong <mmssjj72@naver.com>
2024-08-12 01:41:27 +0900

    feat: HeaderSheet 이동 시 삭제 기능 추가

--- src/containers/common/Header/component/HeaderSheet.tsx
+++ src/containers/common/Header/component/HeaderSheet.tsx
@@ -2,7 +2,7 @@ import { dataPath, menuItems } from '@/containers/common/Header/const/pathData';
 import { Sheet, SheetContent, SheetTrigger } from '@/components/ui/sheet';
 import { CaretDown } from '@phosphor-icons/react';
 import { ReactNode, useState } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate, useSearchParams } from 'react-router-dom';
 
 interface HeaderSheetProps {
   trigger: ReactNode;
@@ -10,9 +10,17 @@ interface HeaderSheetProps {
 
 export function HeaderSheet({ trigger }: HeaderSheetProps) {
   const [expandedCategory, setExpandedCategory] = useState<string | null>(null);
+  const navigate = useNavigate();
+  const [searchParams] = useSearchParams();
 
   const toggleCategory = (category: string) => {
     setExpandedCategory((prevCategory) => (prevCategory === category ? null : category));
+  };
+
+  const handleLinkClick = (path: string) => {
+    searchParams.set('path', path);
+    navigate({ search: searchParams.toString() });
+    window.location.reload();
   };
 
   return (
@@ -38,22 +46,25 @@ export function HeaderSheet({ trigger }: HeaderSheetProps) {
               {expandedCategory === category && (
                 <div className={`flex-center flex flex-col justify-center border-b border-[#E5E7EB] bg-white py-4`}>
                   {items.map((item) => (
-                    <Link
+                    <div
                       key={item.path}
-                      to={item.path}
-                      className={`flex h-[32px] items-center px-4 pl-12 text-base font-medium text-[#4B5563] hover:text-[#9CA3AF]`}
+                      onClick={() => handleLinkClick(item.path)}
+                      className={`flex h-[32px] cursor-pointer items-center px-4 pl-12 text-base font-medium text-[#4B5563] hover:text-[#9CA3AF]`}
                     >
                       {item.name}
-                    </Link>
+                    </div>
                   ))}
                 </div>
               )}
             </div>
           ))}
-          {/* pretier ignore */}
-          <Link to={dataPath} className={`flex h-[64px] items-center border-b border-[#E5E7EB] pl-10 text-[#1F2937]`}>
+          {/* prettier-ignore */}
+          <div
+            onClick={() => handleLinkClick(dataPath)}
+            className={`flex h-[64px] items-center border-b border-[#E5E7EB] pl-10 text-[#1F2937] cursor-pointer`}
+          >
             자료집
-          </Link>
+          </div>
         </div>
       </SheetContent>
     </Sheet>
