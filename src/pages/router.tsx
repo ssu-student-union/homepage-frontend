6629584e30e95fe9892fa234b3fa884e6b5ea288
sejong <mmssjj72@naver.com>
2024-08-13 19:02:26 +0900

    feat: Intro page main pull

--- src/pages/router.tsx
+++ src/pages/router.tsx
@@ -1,7 +1,7 @@
 import { Routes, Route } from 'react-router-dom';
 import { MainPage } from './main/page';
 import { BoardPage } from './boardTest/page';
-import { IntroPage } from './intro/page';
+import { IntroPage } from './Intro/page';
 
 export function MainRouter() {
   return (
