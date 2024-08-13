2a0643ac64162ad81db3241cff7edface439410c
sejong <mmssjj72@naver.com>
2024-08-13 19:38:24 +0900

    feat/#12_intro_page pr 리뷰 1차 반영

--- src/components/Breadcrumb/index.tsx
+++ src/components/Breadcrumb/index.tsx
@@ -4,15 +4,21 @@ import { CaretRight } from '@phosphor-icons/react';
 
 interface BreadcrumbProps {
   // items는 경로 이름(key) : 라우트 경로(value) 구조의 Map 타입 값입니다.
-  // 경로가 따로 없을 시 null 처리해주시면 됩니다. <- 혹시 리액트에서 null을 잘 사용 안하나요?
-  items: Map<string, string | null>;
+  // 경로가 따로 없을 시 null 처리해주시면 됩니다.
+  items?: Map<string, string | null>;
 
   // tailwind 적용을 위한 className props 추가.
   // ex) <Breadcrumb className = "text-sm"/>
   className?: string;
 }
 
-const Breadcrumb = ({ items, className = '' }: BreadcrumbProps) => {
+const Breadcrumb = ({
+  items = new Map<string, string | null>([
+    ['소개', '/intro?category=president&sub-category=intro'],
+    ['총학생회', '/intro?category=president&sub-category=intro'],
+  ]), // 기본값 설정
+  className = '',
+}: BreadcrumbProps) => {
   const pathnames = Array.from(items.keys());
 
   return (
