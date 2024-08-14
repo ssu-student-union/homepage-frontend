5de9b253bd5116855402081c67aa68681b3c8612
Kangnets <kangnets88@gmail.com>
2024-08-15 04:44:13 +0900

    fix: FilterDropDown에 cn() 추가

--- src/components/FilterDropDown/FilterDropDown.tsx
+++ src/components/FilterDropDown/FilterDropDown.tsx
@@ -1,18 +1,19 @@
 import { Select, SelectContent, SelectGroup, SelectItem, SelectTrigger, SelectValue } from '@/components/ui/select';
+import { cn } from '@/libs/utils';
 
-interface FilterDropDownProps {
+interface FilterDropDownProps  extends React.HTMLAttributes<HTMLDivElement>  {
     defaultValue: string
     optionValue: string[]
     onValueChange: (value: string) => void
     value: string
 }
 
-export function FilterDropDown({ defaultValue, optionValue, onValueChange, value }: FilterDropDownProps) {
+export function FilterDropDown({ className, defaultValue, optionValue, onValueChange, value }: FilterDropDownProps) {
   const isSelected = !!value;
 
   return (
-    <Select onValueChange={onValueChange} value={value}>
-      <SelectTrigger className={`h-[67px] w-[411px] pl-9 ${isSelected ? 'text-gray-800' : 'text-gray-500'}`}>
+    <Select  onValueChange={onValueChange} value={value}>
+      <SelectTrigger className={cn(`h-[67px] w-[411px] pl-9 ${isSelected ? 'text-gray-800' : 'text-gray-500'}`,className)}>
         <SelectValue placeholder={defaultValue} />
       </SelectTrigger>
       <SelectContent>
