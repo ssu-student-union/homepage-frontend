f36b43c1a1071b3fd2d38c935fd01df4f921f718
intersoom <sallybig@naver.com>
2024-08-16 01:43:10 +0900

    fix: add style min-w-fit

--- src/components/Category/index.tsx
+++ src/components/Category/index.tsx
@@ -5,7 +5,7 @@ interface CategoryProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
 export function Category({ isActive = false, children, ...props }: CategoryProps) {
   return (
     <button
-      className={`flex h-[38px] items-center justify-center rounded-[32px] border border-gray-800 px-[16px] py-[8px] text-[1.125rem] font-bold transition-colors duration-100 xs:h-[31px] xs:text-[0.875rem] sm:h-[31px] sm:text-[0.875rem] ${
+      className={`flex h-[38px] min-w-fit items-center justify-center rounded-[32px] border border-gray-800 px-[16px] py-[8px] text-[1.125rem] font-bold transition-colors duration-100 xs:h-[31px] xs:text-[0.875rem] sm:h-[31px] sm:text-[0.875rem] ${
         isActive ? 'bg bg-primary text-white' : 'border border-gray-300 bg-white text-gray-700 hover:bg-gray-100'
       } `}
       {...props}
