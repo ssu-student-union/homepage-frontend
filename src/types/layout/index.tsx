a3ff548660995cacdd311813e52d0480bfae0bc2
sejong <mmssjj72@naver.com>
2024-08-17 14:39:58 +0900

    merge: main

--- src/types/layout/index.tsx
+++ src/types/layout/index.tsx
@@ -3,6 +3,7 @@ export interface HeadLayoutProps {
   subtitle: React.ReactNode;
   className?: string;
   borderOff?: boolean; // 감사페이지에 선이 필요 없기에 제거할 수 있게 추가했습니다.
+  searchHidden?: string; // 페이지마다 search바 hidden 기준이 달라서 추가했습니다.
 }
 
 export interface BodyLayoutProps {
@@ -13,6 +14,7 @@ export interface BodyLayoutProps {
   currentPage: number;
   onPageChange: (page: number) => void;
   onWriteClick: () => void;
+  className?: string;
 }
 
 export interface EditLayoutProps {
