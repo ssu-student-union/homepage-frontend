73825e0af250ba5f9c106bebbc545caddc3d42de
dvp-tae <th3016@naver.com>
2024-08-23 16:24:36 +0900

    fix: posttextpetition props, 타입 수정

--- src/components/PostTextPetition/index.tsx
+++ src/components/PostTextPetition/index.tsx
@@ -1,24 +1,34 @@
 import { ThumbsUp } from 'lucide-react';
 import { ACTIVE_TAG, ANSWERED_TAG, CLOSED_TAG, RECEIVED_TAG } from '../StateTag/const';
 import { useResize } from '@/hooks/useResize';
-import { StateTagProps } from '../StateTag/types';
+import { PostListDtoResponse } from './types';
 
-export function PostTextPetition({ current }: StateTagProps) {
+interface PostTextPetitionProps {
+  data: PostListDtoResponse;
+}
+
+export function PostTextPetition({ data }: PostTextPetitionProps) {
   const { width } = useResize();
   const isSmallScreen = width <= 391;
+
   const renderStatusTag = () => {
-    switch (current) {
-      case 'ACTIVE':
+    switch (data?.onGoingStatus) {
+      case '진행중':
         return <ACTIVE_TAG isActive={true} />;
-      case 'RECEIVED':
+      case '접수완료':
         return <RECEIVED_TAG isActive={true} />;
-      case 'ANSWERED':
+      case '답변완료':
         return <ANSWERED_TAG isActive={true} />;
-      case 'CLOSED':
+      case '종료됨':
         return <CLOSED_TAG isActive={true} />;
       default:
         return null;
     }
+  };
+
+  const truncateText = (text: string, maxLength: number) => {
+    if (text.length <= maxLength) return text;
+    return text.slice(0, maxLength).trim() + '…';
   };
 
   return (
@@ -26,19 +36,18 @@ export function PostTextPetition({ current }: StateTagProps) {
       <div className="flex flex-col">
         {renderStatusTag()}
         <div className="mb-[22px] mt-[7px] text-[1.375rem] font-bold text-gray-700 xs:mb-[12px] xs:text-[1rem]">
-          대동제 축제 때 에스파 불러주세요
+          {data?.title && truncateText(data.title, 18)}
         </div>
         <div className="mb-[14px] text-[1.125rem] font-medium text-gray-500 xs:mb-[9px] xs:text-[0.875rem]">
-          대동제가 좋아요. 우리 학교 축제 짱 디혁국 짱짱 우와우와우 히히히 지금이 몇시죠 집에 가고싶어요. YOASIBI가
-          대동제에 올 수 있을까요?
+          {data?.content}
         </div>
         <div className="flex justify-between">
-          <div className="text-[1.125rem] font-normal text-gray-400 xs:text-[0.875rem]">2023/03/21</div>
+          <div className="text-[1.125rem] font-normal text-gray-400 xs:text-[0.875rem]">{data?.date}</div>
           <div className="flex gap-1 text-[#7D7BFF]">
             <span className="cursor-pointer xs:pb-[9px] xs:pt-0">
               <ThumbsUp size={isSmallScreen ? 14 : 22} />
             </span>
-            <span className="text-[1.125rem] font-medium xs:text-[0.875rem]">32</span>
+            <span className="text-[1.125rem] font-medium xs:text-[0.875rem]">{data?.likeCount}</span>
           </div>
         </div>
       </div>
