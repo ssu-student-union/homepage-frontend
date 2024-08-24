767e54c37ada17bf8b44ba6fd8f9a1c9a8d58866
sejong <mmssjj72@naver.com>
2024-08-24 20:48:27 +0900

    feat: add delBoardPosts

--- src/apis/delBoardPosts.ts
+++ src/apis/delBoardPosts.ts
@@ -1,6 +1,6 @@
 import { clientAuth } from './client';
 
-export async function postBoardPosts(boardCode: string, postId: string): Promise<PostBoardPostsResponse> {
+export async function postBoardPosts(boardCode: string, postId: string) {
   const resp = await clientAuth({
     method: 'delete',
     url: `/boards/${boardCode}/posts/${postId}`,
