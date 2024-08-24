3b615c23b8990b2afc47e6697c3fa3aa08d31de3
sejong <mmssjj72@naver.com>
2024-08-24 20:55:35 +0900

    feat: add useDelBoardPosts.ts

---
+++ src/hooks/useDelBoardPosts.ts
@@ -0,0 +1,13 @@
+import { delBoardPosts } from '@/apis/delBoardPosts';
+import { useMutation, UseMutationResult } from '@tanstack/react-query';
+import { AxiosError, AxiosResponse } from 'axios';
+
+export function useDelBoardPosts(): UseMutationResult<
+  AxiosResponse<any>,
+  AxiosError,
+  { boardCode: string; postId: string }
+> {
+  return useMutation({
+    mutationFn: ({ boardCode, postId }) => delBoardPosts(boardCode, postId),
+  });
+}
