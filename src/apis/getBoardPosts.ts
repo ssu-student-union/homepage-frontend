bdddf31b9512557761d0780063a0f21948249229
dvp-tae <th3016@naver.com>
2024-08-26 01:32:07 +0900

    fix: 주석 제거

--- src/apis/getBoardPosts.ts
+++ src/apis/getBoardPosts.ts
@@ -1,8 +1,6 @@
 import { getBoardPostsProps } from '@/types/apis/get';
 import { client } from './client';
 
-// GET:/board/{boardCode}/posts 요청
-
 export const getBoardPosts = async ({ page, take, groupCode, memberCode, category, boardCode }: getBoardPostsProps) => {
   try {
     const response = await client.get(`/board/${boardCode}/posts`, {
