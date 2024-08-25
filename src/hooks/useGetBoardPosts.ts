c69ca9e6de59f5f0b93f6959696ec690331cdd51
dvp-tae <th3016@naver.com>
2024-08-26 01:29:33 +0900

    fix: useGetBoardPosts type fix

--- src/hooks/useGetBoardPosts.ts
+++ src/hooks/useGetBoardPosts.ts
@@ -1,10 +1,10 @@
 import { getBoardPosts } from '@/apis/getBoardPosts';
 import { getBoardPostsProps } from '@/types/apis/get';
-import { PostBoardPostsResponse } from '@/types/apis/post';
+import { PetitionPostsTopLikedResponse } from '@/types/getPetitionTopLiked';
 import { useQuery } from '@tanstack/react-query';
 
 export const useGetBoardPosts = ({ page, take, groupCode, memberCode, category, boardCode }: getBoardPostsProps) => {
-  return useQuery<PostBoardPostsResponse>({
+  return useQuery<PetitionPostsTopLikedResponse>({
     queryKey: ['get-board-boardCode-posts', page, take, groupCode, memberCode, category, boardCode],
     queryFn: () =>
       getBoardPosts({
