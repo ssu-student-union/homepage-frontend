65c759bc01d2fcf84b88f5ef0a4ee1ae45d299a7
intersoom <sallybig@naver.com>
2024-08-26 00:16:29 +0900

    feat: notice section link to api (in progress)

--- src/pages/main/containers/NoticeSection.tsx
+++ src/pages/main/containers/NoticeSection.tsx
@@ -2,18 +2,29 @@ import { BoardSelector } from '@/components/Board/BoardSelector';
 import { Spacing } from '@/components/Spacing';
 import { MainSubcategories } from '../const';
 import { useBoardSelect } from '@/hooks/useBoardSelect';
-import { MainSubcategoriesType } from '../type';
 import { Button } from '@/components/ui/button';
 import { PostCardNotice } from '@/components/PostCard/PostCardNotice';
 import { useResize } from '@/hooks/useResize';
+import { useGetBoardPosts } from '@/hooks/useGetBoardPosts';
+import { GetNoticeBoardPostsResponse } from '@/types/getBoardPosts';
+import { useMemo } from 'react';
+import { MainNoticesType } from '@/types';
 
-interface NoticeSectionProps {
-  noticeCount: number;
-}
-
-const NoticeSection = ({ noticeCount }: NoticeSectionProps) => {
-  const { selectedSubcategories, onSubcategorySelect } = useBoardSelect<MainSubcategoriesType>(MainSubcategories[0]);
+const NoticeSection = () => {
+  const { selectedSubcategories, onSubcategorySelect } = useBoardSelect<MainNoticesType>(MainSubcategories[0]);
   const { width } = useResize();
+  const { data, isLoading } = useGetBoardPosts<GetNoticeBoardPostsResponse>({
+    boardCode: '공지사항게시판',
+    page: 1,
+    take: 4,
+    category: selectedSubcategories === '전체' ? null : selectedSubcategories,
+  });
+  const noticeCount = useMemo(() => {
+    if (!data?.data.pageInfo.totalElements) {
+      return undefined; // 로딩 중일 때는 undefined를 반환하여 UI가 바뀌지 않도록 합니다.
+    }
+    return data?.data.pageInfo.totalElements; // 데이터가 로딩되면 새로운 데이터로 UI를 업데이트합니다.
+  }, [data?.data.pageInfo.totalElements, isLoading]);
 
   return (
     <section className="w-full whitespace-nowrap">
@@ -37,40 +48,72 @@ const NoticeSection = ({ noticeCount }: NoticeSectionProps) => {
         {/* xs */}
         {width < 390 ? (
           <div className="flex w-[calc(100dvw-3.125rem)] gap-[1.063rem] overflow-x-scroll pr-[1.063rem] scrollbar-hide">
-            {Array.from({ length: 3 }).map((_) => (
-              <PostCardNotice></PostCardNotice>
+            {data?.data.postListResDto.map((notice) => (
+              <PostCardNotice
+                badgeType={notice.status}
+                key={notice.postId}
+                imgUrl={notice.thumbNail}
+                title={notice.title}
+                profileName={notice.author}
+              ></PostCardNotice>
             ))}
           </div>
         ) : null}
         {/* sm, md */}
         {width < 1080 && width >= 390 ? (
           <div className="flex w-[calc(100dvw-3.125rem)] gap-[1.063rem] overflow-x-scroll pr-[1.063rem] scrollbar-hide">
-            {Array.from({ length: 3 }).map((_) => (
-              <PostCardNotice></PostCardNotice>
+            {data?.data.postListResDto.map((notice) => (
+              <PostCardNotice
+                badgeType={notice.status}
+                key={notice.postId}
+                imgUrl={notice.thumbNail}
+                title={notice.title}
+                profileName={notice.author}
+              ></PostCardNotice>
             ))}
           </div>
         ) : null}
         {/* lg */}
         {width < 1440 && width >= 1080 ? (
           <div className="flex h-fit w-full justify-between gap-[26px]">
-            {Array.from({ length: 2 }).map((_) => (
-              <PostCardNotice></PostCardNotice>
+            {data?.data.postListResDto.map((notice) => (
+              <PostCardNotice
+                badgeType={notice.status}
+                key={notice.postId}
+                imgUrl={notice.thumbNail}
+                title={notice.title}
+                profileName={notice.author}
+              ></PostCardNotice>
             ))}
           </div>
         ) : null}
         {/* xl */}
         {width >= 1440 && width < 1920 ? (
           <div className="flex h-fit w-full justify-between">
-            {Array.from({ length: 3 }).map((_) => (
-              <PostCardNotice></PostCardNotice>
+            {data?.data.postListResDto.map((notice) => (
+              <PostCardNotice
+                badgeType={notice.status}
+                key={notice.postId}
+                imgUrl={notice.thumbNail}
+                title={notice.title}
+                profileName={notice.author}
+              ></PostCardNotice>
             ))}
           </div>
         ) : null}
         {/* xxl */}
         {width >= 1920 ? (
           <div className="flex h-fit w-full justify-between">
-            {Array.from({ length: 4 }).map((_) => (
-              <PostCardNotice></PostCardNotice>
+            {data?.data.postListResDto.map((notice) => (
+              <PostCardNotice
+                badgeType={notice.status}
+                key={notice.postId}
+                imgUrl={notice.thumbNail}
+                title={notice.title}
+                profileName={notice.author}
+                date={notice.date}
+                subtitle={notice.content}
+              ></PostCardNotice>
             ))}
           </div>
         ) : null}
