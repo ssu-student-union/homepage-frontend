33e010022274950ec157f77baa32562c5da09879
dvp-tae <th3016@naver.com>
2024-08-30 06:48:50 +0900

    fix: image delete 수정

--- src/pages/main/containers/PetitionSection.tsx
+++ src/pages/main/containers/PetitionSection.tsx
@@ -1,23 +1,22 @@
 import { PostTextPetition } from '@/components/PostTextPetition';
 import { Spacing } from '@/components/Spacing';
 import { useGetPetitionTopLiked } from '@/hooks/useGetPetitionPostsTopLiked';
+import { useNavigate } from 'react-router-dom';
 
 const PetitionSection = () => {
+  const navigate = useNavigate();
   const { data } = useGetPetitionTopLiked({ page: 0, take: 2 });
+  const handlePostDetail = (id: number) => {
+    navigate(`/petition-notice/${id}`);
+  };
 
   return (
     <section className="w-full">
       <h1 className="text-[2rem] font-bold xs:text-[1.25rem]">인기청원</h1>
       <Spacing size={18} direction="vertical" />
-      <div className="scrollbar-hide flex w-full gap-[1.5rem] overflow-x-scroll xs:pr-[1.5rem] sm:pr-[1.5rem]">
+      <div className="flex w-full gap-[1.5rem] overflow-x-scroll scrollbar-hide xs:pr-[1.5rem] sm:pr-[1.5rem]">
         {data?.data.postListResDto.map((petitionData) => (
-          <PostTextPetition
-            data={petitionData}
-            key={petitionData.postId}
-            onClick={function (): void {
-              throw new Error('Function not implemented.');
-            }}
-          />
+          <PostTextPetition data={petitionData} key={petitionData.postId} onClick={handlePostDetail} />
         ))}
       </div>
     </section>
