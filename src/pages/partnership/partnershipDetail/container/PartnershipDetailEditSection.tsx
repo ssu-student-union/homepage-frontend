6aa146c949afb2cb6edd05b6b0dbe87873585413
intersoom <sallybig@naver.com>
2024-09-02 00:32:10 +0900

    fix: fix partnership delete function

--- src/pages/partnership/partnershipDetail/container/PartnershipDetailEditSection.tsx
+++ src/pages/partnership/partnershipDetail/container/PartnershipDetailEditSection.tsx
@@ -1,7 +1,6 @@
 import { DeleteButton, EditButton, ListButton } from '@/components/Buttons/BoardActionButtons';
 import { useDelBoardPosts } from '@/hooks/useDelBoardPosts';
 import { useNavigate } from 'react-router-dom';
-import { deleteHandler } from '../utils/deleteHandler';
 
 interface PartnershipDetailEditProps {
   boardCode: string;
@@ -11,15 +10,15 @@ interface PartnershipDetailEditProps {
 
 export function PartnershipDetailEditSection({ boardCode, postId, fileurl = [] }: PartnershipDetailEditProps) {
   const navigate = useNavigate();
-  const mutation = useDelBoardPosts();
+  const { mutate } = useDelBoardPosts();
 
   return (
     <div className="flex w-full justify-end py-[60px] sm:py-[40px]">
       <div className="flex w-[420px] flex-row items-end justify-between xs:h-[150px] xs:flex-col">
         <DeleteButton
           onClick={() => {
-            deleteHandler({ boardCode, postId, fileurl, mutation });
-            navigate(`homepage-frontend/partnership`);
+            mutate({ boardCode, postId, fileurl });
+            navigate('/homepage-frontend/partnership');
           }}
         />
         <EditButton
