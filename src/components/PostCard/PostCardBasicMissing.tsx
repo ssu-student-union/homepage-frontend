7600d718a15c06871f9ed3ed2b48febe47a8a66f
jongse7 <mmssjj72@naver.com>
2024-09-19 00:00:12 +0900

    feat: 글자수 제한 적용

--- src/components/PostCard/PostCardBasicMissing.tsx
+++ src/components/PostCard/PostCardBasicMissing.tsx
@@ -1,18 +1,19 @@
-import { cn } from '@/libs/utils';
+import useTruncateText from '@/hooks/useTruncateText';
 import { Badge } from '../ui/badge';
 import { Size } from './const/state';
 import { getStyles } from './const/style';
+import { cn } from '@/libs/utils';
 
 interface PostCardProps {
-  imgUrl?: string; // 게시글 이미지 url
-  title?: string; // 게시글 제목
-  subtitle?: string; // 게시글 내용
-  date?: string; // 게시글 게시 날짜
-  badgeType?: 'Emergency' | 'New' | 'Default'; // postCard 배지 종류 - 긴급, NEW!, 없음
-  cardType: 'Basic' | 'Missing'; // PostCardBasic | PostCardMissing 컴포넌트 선택
-  size?: Size; // 페이지마다 반응형 기준이 다름 -> 자동 반응형이 아니라 수동으로 적용하도록 제작
-  profileImg?: string | null; // 프로필 이미지
-  profileName?: string; // 계정명
+  imgUrl?: string;
+  title?: string;
+  subtitle?: string;
+  date?: string;
+  badgeType?: 'Emergency' | 'New' | 'Default';
+  cardType: 'Basic' | 'Missing';
+  size?: Size;
+  profileImg?: string | null;
+  profileName?: string;
   onClick?: () => void;
   className?: string;
 }
@@ -32,6 +33,12 @@ const PostCard = ({
 }: PostCardProps) => {
   const styles = getStyles(size);
 
+  const maxTitleLength = size === Size.default ? 17 : 30;
+  const maxSubtitleLength = size === Size.default ? 24 : 50;
+
+  const truncatedTitle = useTruncateText(title, maxTitleLength);
+  const truncatedSubtitle = useTruncateText(subtitle, maxSubtitleLength);
+
   return (
     <div
       className={cn(
@@ -48,8 +55,8 @@ const PostCard = ({
         <img alt="image" src={imgUrl} className={`rounded-[8px] bg-gray-200 object-cover ${styles.image}`} />
         <div className="w-full flex-col">
           <div className={`flex flex-col ${styles.title}`}>
-            <p className={`line-clamp-2 font-semibold`}>{title}</p>
-            <p className={`line-clamp-2 font-normal text-gray-500 ${styles.subtitle}`}>{subtitle}</p>
+            <p className={`line-clamp-2 font-semibold`}>{truncatedTitle}</p>
+            <p className={`line-clamp-2 font-normal text-gray-500 ${styles.subtitle}`}>{truncatedSubtitle}</p>
           </div>
           <hr className={`w-full border border-gray-300 ${styles.hr}`} />
           <div className={`flex items-end gap-1 font-normal text-gray-500 ${styles.date}`}>
@@ -57,7 +64,7 @@ const PostCard = ({
               <div className="flex items-center gap-1">
                 {profileImg && <img alt="logo" src={profileImg} className={`pr-0.5 ${styles.profileImg}`} />}
                 {profileName && <span>{profileName}</span>}
-                {profileName && <span>·</span>} {/* 프로필 이름이 있으면 center dot 렌더링 */}
+                {profileName && <span>·</span>}
               </div>
             )}
             <span>{date}</span>
@@ -68,10 +75,7 @@ const PostCard = ({
   );
 };
 
-// PostCardBasic => imgUrl, title, subtitle, date, badgeType, size, profileImg, profileName 속성 기입해서 사용
 export const PostCardBasic = (props: Omit<PostCardProps, 'cardType'>) => <PostCard cardType="Basic" {...props} />;
-
-// PostCardMissing => imgUrl, title, subtitle, date, size 속성 기입해서 사용
 export const PostCardMissing = (
   props: Omit<PostCardProps, 'cardType' | 'badgeType' | 'profileImg' | 'profileName'>
 ) => <PostCard cardType="Missing" badgeType="Default" {...props} />;
