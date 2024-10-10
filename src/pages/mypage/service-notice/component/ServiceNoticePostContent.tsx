5efde76ecbf1d3f20ea3d356c6e3e070a200da2d
ParkSumin <fhsjdvs@gmail.com>
2024-10-10 20:52:08 +0900

    Feat: ServiceNoticePostContent

--- src/pages/mypage/service-notice/component/ServiceNoticePostContent.tsx
+++ src/pages/mypage/service-notice/component/ServiceNoticePostContent.tsx
@@ -1,7 +1,14 @@
 import { useResize } from '@/hooks/useResize';
+import { Badge } from '@/components/ui/badge';
 import { useEffect, useState } from 'react';
 
-export function ServiceNoticePostContent() {
+interface ServiceNoticePostContentProps {
+  title: string;
+  date: string;
+  Emergency: boolean;
+}
+
+export function ServiceNoticePostContent({title, date, Emergency}: ServiceNoticePostContentProps) {
   const [contentWidth, setContentWidth] = useState(1520); // 초기값은 1520px로 설정
   const { width } = useResize();
 
@@ -20,9 +27,21 @@ export function ServiceNoticePostContent() {
   }, [width]);
 
   return (
-    <div className="flex justify-between items-center h-[64px] border-b-[1px]" style={{ width: `${contentWidth}px` }}>
-      <div className='text-[18px] font-[500] font-pretendard ml-[50px]'>[점검] 총학생회 홈페이지 점검으로 인한 기능 제한</div>
-      <div>2023/10/02</div>
-    </div>
+    <>
+      <div className="flex h-[64px] border-b-[1px]" style={{ width: `${contentWidth}px` }}>
+        {Emergency ? 
+          <Badge variant="Emergency" className="relative top-[22px]">
+            긴급
+          </Badge> :
+          <div className='w-[54px] h-[23px]'></div>
+        }
+        <div className="flex w-full items-center justify-between">
+          <div className="ml-[20px] font-pretendard text-[18px] font-[500]">
+            {title}
+          </div>
+          <div>{date}</div>
+        </div>
+      </div>
+    </>
   );
 }
--- src/pages/mypage/service-notice/page.tsx
+++ src/pages/mypage/service-notice/page.tsx
@@ -4,6 +4,11 @@ import { ServiceNoticePostContent } from './component/ServiceNoticePostContent';
 import Pagination from '@/components/Pagination';
 
 export function ServiceNoticePage() {
+
+const onPageChange = (page: number) => {
+  console.log(page);
+};
+
   return (
     <>
       <div>
@@ -12,17 +17,18 @@ export function ServiceNoticePage() {
         </div>
         <HeadLayout title="서비스 공지사항" subtitle="홈페이지 개발자의 공지사항을 업로드합니다" searchHidden={true} />
         <div className="flex flex-col justify-center items-center mt-[64px] mb-[100px]">
-          <ServiceNoticePostContent />
-          <ServiceNoticePostContent />
-          <ServiceNoticePostContent />
-          <ServiceNoticePostContent />
-          <ServiceNoticePostContent />
-          <ServiceNoticePostContent />
-          <ServiceNoticePostContent />
-          <ServiceNoticePostContent />
-          <ServiceNoticePostContent />
+          <ServiceNoticePostContent title='[점검] 서비스 공지사항 페이지 작업' date={'2020/10/25'} Emergency={true}/>
+          <ServiceNoticePostContent title='[점검] 서비스 공지사항 페이지 작업' date={'2020/10/25'} Emergency={true}/>
+          <ServiceNoticePostContent title='[점검] 서비스 공지사항 페이지 작업' date={'2020/10/25'} Emergency={false}/>
+          <ServiceNoticePostContent title='[점검] 서비스 공지사항 페이지 작업' date={'2020/10/25'} Emergency={false}/>
+          <ServiceNoticePostContent title='[점검] 서비스 공지사항 페이지 작업' date={'2020/10/25'} Emergency={false}/>
+          <ServiceNoticePostContent title='[점검] 서비스 공지사항 페이지 작업' date={'2020/10/25'} Emergency={false}/>
+          <ServiceNoticePostContent title='[점검] 서비스 공지사항 페이지 작업' date={'2020/10/25'} Emergency={false}/>
+          <ServiceNoticePostContent title='[점검] 서비스 공지사항 페이지 작업' date={'2020/10/25'} Emergency={false}/>
+          <ServiceNoticePostContent title='[점검] 서비스 공지사항 페이지 작업' date={'2020/10/25'} Emergency={false}/>
+          <ServiceNoticePostContent title='[점검] 서비스 공지사항 페이지 작업' date={'2020/10/25'} Emergency={false}/>
         </div>
-        <Pagination totalPages={1} currentPage={1} />
+        <Pagination totalPages={1} currentPage={1} onPageChange={onPageChange}/>
       </div>
     </>
   );
