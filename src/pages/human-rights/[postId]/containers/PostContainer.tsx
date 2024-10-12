828ad567fe405b95b08d4eb27fa938c1962d35f3
EATSTEAK <me@eatsteak.dev>
2024-10-12 16:49:42 +0900

    feat: `PostContainer`와 `PostHeader` 컴포넌트가 `className` prop을 받도록 수정

--- src/pages/human-rights/[postId]/containers/PostContainer.tsx
+++ src/pages/human-rights/[postId]/containers/PostContainer.tsx
@@ -1,12 +1,16 @@
 import { ReactNode } from 'react';
+import { cn } from '@/libs/utils.ts';
 
 interface PostContentProps {
+  className?: string;
   children: ReactNode;
 }
 
-export function PostContainer({ children }: PostContentProps) {
+export function PostContainer({ className, children }: PostContentProps) {
   return (
-    <section className="flex justify-center px-10 md:px-[72px] lg:px-[200px] xl:px-[200px] xxl:px-[200px]">
+    <section
+      className={cn('flex justify-center px-10 md:px-[72px] lg:px-[200px] xl:px-[200px] xxl:px-[200px]', className)}
+    >
       <div className="w-full max-w-[1040px] py-20">{children}</div>
     </section>
   );
--- src/pages/human-rights/[postId]/containers/PostHeader.tsx
+++ src/pages/human-rights/[postId]/containers/PostHeader.tsx
@@ -1,20 +1,24 @@
 import dayjs from 'dayjs';
 import Breadcrumb from '@/components/Breadcrumb';
 import { User } from '@phosphor-icons/react';
+import { cn } from '@/libs/utils.ts';
 
 interface PostHeaderProps {
   title: string;
   authorName: string;
   createdAt: Date;
   breadcrumbItems: [string, string | null][];
+  className?: string;
 }
 
-export function PostHeader({ title, authorName: author, createdAt, breadcrumbItems }: PostHeaderProps) {
+export function PostHeader({ title, authorName: author, createdAt, breadcrumbItems, className }: PostHeaderProps) {
   const breadcrumbMap = new Map<string, string | null>(breadcrumbItems);
   const formattedDate = dayjs(createdAt).format('YYYY/MM/DD HH:mm');
 
   return (
-    <header className="flex justify-center px-10 md:px-[72px] lg:px-[200px] xl:px-[200px] xxl:px-[200px]">
+    <header
+      className={cn('flex justify-center px-10 md:px-[72px] lg:px-[200px] xl:px-[200px] xxl:px-[200px]', className)}
+    >
       <div className="mb-6 flex w-full max-w-[1040px] flex-col gap-2">
         <Breadcrumb items={breadcrumbMap} className="mb-6" />
         <h1 className="text-2xl font-bold text-black">{title}</h1>
