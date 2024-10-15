e08f8f7b22acca174f92e527222b70bcf57bde97
EATSTEAK <me@eatsteak.dev>
2024-10-15 23:41:52 +0900

    feat: 기본적인 레이아웃 작업을 위한 `ArticleHeader`, `ArticleFooter`, `Container` 컨테이너 생성
    TODO: 이 컴포넌트들은 페이지 공통으로 사용할 수 있는 레이아웃이므로 공통 컴포넌트로 이동하는 것을 고려

---
+++ src/pages/human-rights/edit/containers/ArticleFooter.tsx
@@ -0,0 +1,17 @@
+import { cn } from '@/libs/utils.ts';
+import { ReactNode } from 'react';
+
+interface ArticleFooterProps {
+  children?: ReactNode;
+  className?: string;
+}
+
+export function ArticleFooter({ children, className }: ArticleFooterProps) {
+  return (
+    <footer
+      className={cn('flex justify-center px-10 md:px-[72px] lg:px-[200px] xl:px-[200px] xxl:px-[200px]', className)}
+    >
+      <div className="mb-6 flex w-full max-w-[1040px] flex-col gap-2">{children}</div>
+    </footer>
+  );
+}
