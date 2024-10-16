b9891a8133d4bd0b1891151ab9ac51f8f8c9f2dc
EATSTEAK <me@eatsteak.dev>
2024-10-16 11:07:25 +0900

    chore: 불필요한 주석 제거

--- src/pages/human-rights/edit/components/FileInputs.tsx
+++ src/pages/human-rights/edit/components/FileInputs.tsx
@@ -12,7 +12,7 @@ export function FileInputs({ className, files, onChange }: FileInputsProps) {
   const [innerFiles, setInnerFiles] = useState<File[]>([]);
 
   useEffect(() => {
-    // Do not trigger onChange if files is controlled from outside.
+    // Do not trigger onChange if files are controlled from outside.
     // 외부에서 files 값이 변경될 경우 re-render는 수행하지만 onChange 이벤트를 발생시키지 않습니다.
     if (files) setInnerFiles(files);
     else setInnerFiles([]);
@@ -24,7 +24,7 @@ export function FileInputs({ className, files, onChange }: FileInputsProps) {
       const newFiles = [...innerFiles, file];
       setInnerFiles(newFiles);
       if (onChange) onChange(newFiles);
-      // Remove file in new file input to keep input fresh.
+      // Remove the existing file in the new file input to keep input fresh.
       // 새 파일을 입력받는 input 의 파일을 제거하여 파일이 추가된 상태로 보이지 않도록 합니다.
       evt.currentTarget.files = new DataTransfer().files;
     }
