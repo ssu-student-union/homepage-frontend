1b3ee916b653e7fdc2d783423ba7e8a52247ece6
EATSTEAK <me@eatsteak.dev>
2024-10-16 11:02:08 +0900

    fix: `FrontmatterInput`의 `disabled` 상태가 표현되도록 배경 색상 수정

--- src/pages/human-rights/edit/components/FrontmatterInput.tsx
+++ src/pages/human-rights/edit/components/FrontmatterInput.tsx
@@ -4,7 +4,13 @@ import { cn } from '@/libs/utils.ts';
 // TODO: 기존 Input 디자인과 상이하여 새로운 컴포넌트인 `FrontmatterInput` 생성, 추후 디자인팀과 논의하여 다른 페이지의 폼과 동일한 형태가 되도록 리팩토링 필요
 const FrontmatterInput = forwardRef<HTMLInputElement, InputHTMLAttributes<HTMLInputElement>>(
   ({ className, ...props }, ref) => {
-    return <input className={cn('w-fit rounded-md border-2 border-[#CDCDCD]', className)} ref={ref} {...props} />;
+    return (
+      <input
+        className={cn('w-fit rounded-md border-2 border-[#CDCDCD] disabled:bg-[#CDCDCD]', className)}
+        ref={ref}
+        {...props}
+      />
+    );
   }
 );
 
