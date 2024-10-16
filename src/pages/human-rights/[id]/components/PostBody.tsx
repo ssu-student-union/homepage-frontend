0e9c086a4ac1a3628e62ea96f88f60b9ad362fe5
Hyomin Koo <me@eatsteak.dev>
2024-10-16 19:27:11 +0900

    fix: 게시물의 content가 마크다운을 이용해 정상적으로 렌더링되도록 수정

--- src/pages/human-rights/[id]/components/PostBody.tsx
+++ src/pages/human-rights/[id]/components/PostBody.tsx
@@ -1,5 +1,6 @@
 import { FileResponse } from '@/types/apis/get';
 import { Attachment } from '@/pages/human-rights/[id]/components/Attachment.tsx';
+import { ContentViewer } from '@/pages/human-rights/[id]/components/ContentViewer.tsx';
 
 interface PostBodyProps {
   content: string;
@@ -7,15 +8,11 @@ interface PostBodyProps {
 }
 
 export function PostBody({ content, files }: PostBodyProps) {
-  const images = files.filter((file) => file.fileType === 'images');
   const attachments = files.filter((file) => file.fileType === 'files');
   return (
     <>
-      <section className="mb-10">{content}</section>
-      <section className="mb-10 flex flex-col gap-4">
-        {images.map((image) => (
-          <img key={image.postFileId} className="w-fit rounded-xs" src={image.fileUrl} alt={image.fileName} />
-        ))}
+      <section className="mb-10">
+        <ContentViewer content={content} />
       </section>
       <section className="mb-10 flex flex-col gap-4">
         {attachments.map((attachment) => (
