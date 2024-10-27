cf807525f8c889c5dd51f3955bbe24cbd918f93e
rail <78732904+jongse7@users.noreply.github.com>
2024-10-28 00:48:29 +0900

    fix: 이미지 가로 고정 및 제휴게시판 게시글 레이아웃 조정

---
+++ src/pages/partnership/hook/usePartnership.ts
@@ -0,0 +1,46 @@
+import { useGetBoardPostSearch } from '@/hooks/useGetBoardPostSearch';
+import { useResponseBoard } from '@/hooks/useResponseBoard';
+import { useCurrentPage } from '@/hooks/useCurrentPage';
+import { useRecoilValue } from 'recoil';
+import { SearchState } from '@/recoil/atoms/atom';
+import { useEffect } from 'react';
+import { useQueryClient } from '@tanstack/react-query';
+import { GetPartnershipBoardPostsResponse } from '@/types/getPartnershipBoardPosts';
+import { PartnershipSubcategories } from '../const';
+
+export function usePartnership(boardCode: string, category: string) {
+  const { itemsPerPage } = useResponseBoard();
+  const { currentPage, handlePageChange } = useCurrentPage();
+
+  const searchQuery = useRecoilValue(SearchState);
+
+  const subcategories = Object.values(PartnershipSubcategories).filter(Boolean) as string[];
+  const selectedCategory = category === '전체' ? null : category;
+
+  const queryClient = useQueryClient();
+  const { data, isLoading, isError, refetch } = useGetBoardPostSearch<GetPartnershipBoardPostsResponse>({
+    boardCode,
+    take: itemsPerPage,
+    page: currentPage - 1,
+    category: selectedCategory ?? undefined,
+    q: searchQuery,
+  });
+
+  useEffect(() => {
+    if (!queryClient.getQueryData(['get-board-boardCode-posts-search'])) {
+      refetch();
+    }
+  }, [queryClient, refetch]);
+
+  const totalPages: number = data?.data?.pageInfo?.totalPages ?? 0;
+
+  return {
+    data,
+    totalPages,
+    currentPage,
+    handlePageChange,
+    subcategories,
+    isLoading,
+    isError,
+  };
+}
