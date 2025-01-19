7e4c6fcdf4e2f7adead097390b0e2192c69bc815
김혜령 <92776341+h-ye-ryoung@users.noreply.github.com>
2025-01-20 02:54:10 +0900

    chore: 임시 자료집 제거 후 자료집 경로 연 결 (#374)

--- src/pages/data/page.tsx
+++ src/pages/data/page.tsx
@@ -1,3 +1,13 @@
+import { BodyLayout } from '@/template/BodyLayout';
+import SortLayout from '@/template/data/SortLayout';
+import { HeadLayout } from '@/template/HeadLayout';
+
 export default function DataPage() {
-  return <div>page</div>;
+  return (
+    <>
+      <HeadLayout title="자료집"></HeadLayout>
+      <SortLayout></SortLayout>
+      <BodyLayout></BodyLayout>
+    </>
+  );
 }
