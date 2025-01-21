9299a95a49f783c1389d8fbf626194e263693c40
rail <78732904+jongse7@users.noreply.github.com>
2025-01-21 15:40:25 +0900

    fix: d-day 변경 (#375)

--- src/containers/common/Header/component/DropDownMenu.tsx
+++ src/containers/common/Header/component/DropDownMenu.tsx
@@ -15,7 +15,6 @@ interface DropDownMenuProps {
 
 const DropDownMenu = ({ items, bgColor, textColor, hoverBgColor }: DropDownMenuProps) => {
   return (
-    // prettier ignore
     <div className={`absolute end-0 left-0 w-40 rounded-xs font-semibold shadow-md ${bgColor} mt-1.5`}>
       {items.map((item) => (
         <Link
