86b7602fa40bdf4d13bd8226cfdf1de288119fea
EATSTEAK <me@eatsteak.dev>
2025-02-15 16:45:31 +0900

    refactor: `FileDownButton`에서 `a` 태그 사용

--- src/components/File/FileDownButton.tsx
+++ src/components/File/FileDownButton.tsx
@@ -1,31 +1,23 @@
 import { cn } from '@/libs/utils';
 import { FileResponse } from '@/schemas/post.ts';
+import { buttonVariants } from '@/components/ui/button.tsx';
 
 // 자료집 '/data'에 쓰이는 FileDownButton입니다.
 
-export default function FileDownButton({ file, className = '' }: { file: FileResponse; className?: string }) {
-  const handleDownload = (event: React.MouseEvent) => {
-    event.stopPropagation();
-    event.preventDefault();
-    const link = document.createElement('a');
-    link.href = file.fileUrl;
-    link.download = file.fileName;
-    document.body.appendChild(link);
-    link.click();
-    document.body.removeChild(link);
-  };
-
+export default function FileDownButton({ file, className }: { file: FileResponse; className?: string }) {
   return (
-    <button
+    <a
       className={cn(
+        buttonVariants(),
         'h-[31px] w-[143px] rounded-[9px] bg-gray-200 text-[16px] font-medium text-gray-600',
         'cursor-pointer overflow-hidden text-ellipsis whitespace-nowrap px-2',
         className
       )}
       title={file.fileType}
-      onClick={handleDownload}
+      href={file.fileUrl}
+      download={file.fileName}
     >
       {file.fileType}
-    </button>
+    </a>
   );
 }
