ab9c2a93e7e79a19099a00766d48f46cd0672e89
naraeng <nauurr@naver.com>
2025-03-01 19:21:27 +0900

    Feat: user posts api

--- src/apis/getUserPosts.ts
+++ src/apis/getUserPosts.ts
@@ -3,10 +3,8 @@ import { AxiosResponse } from 'axios';
 import { clientAuth } from './client';
 
 export const getUserPosts = async (page: number, take: number) => {
-  const accessToken = localStorage.getItem('accessToken');
-  console.log(accessToken);
   const response: AxiosResponse<GetUserPostsResponse> = await clientAuth({
-    baseURL: 'http://13.125.101.7:8080',
+    baseURL: import.meta.env.VITE_API_URL,
     url: `board/mypost?page=${page}&take=${take}`,
     method: 'get',
   });
