7df3575c337a0ef3a8a3776fbe8a3cd597c35540
Hyomin Koo <me@eatsteak.dev>
2025-03-25 08:53:30 +0900

    feat: mixpanel sdk 설치 (#476)
    ## 1️⃣ 작업 내용 Summary
    
    - resolved #475 
    
    ## 2️⃣ 추후 작업할 내용
    
    - 추가 Testimony 설정하여 주요 지표 수집
    
    ## 3️⃣ 체크리스트
    
    - [X] `develop` 브랜치의 최신 코드를 `pull` 받았나요?
    
    ---------
    
    Co-authored-by: naraeng <108282052+naraeng@users.noreply.github.com>

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,15 @@
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
 import '@/translate/i18n';
+//Import Mixpanel SDK
+import mixpanel from 'mixpanel-browser';
+
+// Near entry of your product, init Mixpanel
+mixpanel.init(import.meta.env.VITE_MIXPANEL_TOKEN, {
+  debug: import.meta.env.DEV,
+  track_pageview: true,
+  persistence: 'localStorage',
+});
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   // <React.StrictMode>
