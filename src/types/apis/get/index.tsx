c1a5b1e95eac81a8caefde412fecc8a36d9aca0a
naraeng <108282052+naraeng@users.noreply.github.com>
2025-03-29 04:01:40 +0900

    feat : 마이페이지 작성글 조회 및 검색 api 연동 (#434)
    ## 1️⃣ 작업 내용 Summary
    - 마이페이지 작성글보기 페이지네이션 및 api 연동 코드 추가
    - 마이페이지 작성글 검색 api 연동 코드 추가
    
    - 이전 리뷰 일부 반영
    1. shadcn/ui 컴포넌트 사용, lucide-react 아이콘 사용
    2. early return 처리
    3. mypage/profile -> react-hook-form 사용
    4. 타입 대문자로 수정
    5. api 연동 코드 수정 (clientAuth 사용)
    
    - resolved #399 
    
    ## 2️⃣ 추후 작업할 내용
    - zod가 아직 익숙치 않아서 추후 적용 예정입니다.
    
    ## 3️⃣ 체크리스트
    
    - [x] `develop` 브랜치의 최신 코드를 `pull` 받았나요?

--- src/types/apis/get/index.tsx
+++ src/types/apis/get/index.tsx
@@ -71,26 +71,24 @@ export interface getBoardPostsProps {
   q?: string;
 }
 
-export interface getUserProfileResponse {
-  data: {
-    name: string;
-    nickname?: string;
-    account?: string;
-    studentId?: string;
-    majorCode?: string;
-    memberCode: string;
-    isUnion: boolean;
-  };
+export interface GetUserProfileResponse {
+  name: string;
+  nickname?: string;
+  account?: string;
+  studentId?: string;
+  majorCode?: string;
+  memberCode: string;
+  isUnion: boolean;
 }
 
-export interface patchUserProfileRequest {
+export interface PatchUserProfileRequest {
   nickname: string;
   currentPassword: string;
   newPassword: string;
   confirmNewPassword: string;
 }
 
-export interface patchUserProfileResponse {
+export interface PatchUserProfileResponse {
   name: string;
   nickname: string;
   account: string;
@@ -98,4 +96,24 @@ export interface patchUserProfileResponse {
   majorCode: string | null;
   memberCode: string;
   isUnion: boolean;
+}
+export interface PostListResDto {
+  postId: number;
+  title: string;
+  content: string;
+  date: string;
+  commentCount: number;
+  boardCode: string;
+}
+
+export interface GetUserPostsResponse {
+  postListResDto: PostListResDto[];
+  pageInfo: UserPostsPageInfo;
+}
+
+export interface UserPostsPageInfo {
+  pageNum: number;
+  pageSize: number;
+  totalElements: number;
+  totalPages: number;
 }
