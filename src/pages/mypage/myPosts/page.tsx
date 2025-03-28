4fa8ab684f5d06cf8e9f3b70607a8faefc795cc6
naraeng <108282052+naraeng@users.noreply.github.com>
2025-03-29 04:01:40 +0900

    feat : 마이페이지 작성글 조회 및 검색 api 연동 (#434)
    ## 1️⃣ 작업 내용 Summary
    - 마이페이지 작성글보기 페이지네이션 및 api 연동 코드 추가
    - 마이페이지 작성글 검색 api 연동 코드 추가
    
    - 이전 리뷰 일부 반영
    1. shadcn/ui 컴포넌트 사용, lucide-react 아이콘 사용
    2. early return 처리
    3. mypage/profile -> react-hook-form 사용
    4. 타입 대문자로 수정
    5. api 연동 코드 수정 (clientAuth 사용)
    
    - resolved #399 
    
    ## 2️⃣ 추후 작업할 내용
    - zod가 아직 익숙치 않아서 추후 적용 예정입니다.
    
    ## 3️⃣ 체크리스트
    
    - [x] `develop` 브랜치의 최신 코드를 `pull` 받았나요?

--- src/pages/mypage/myPosts/page.tsx
+++ src/pages/mypage/myPosts/page.tsx
@@ -1,13 +1,87 @@
 import { useState } from 'react';
 import UserContainer from '../component/UserContainer';
 import { MyPostsContent } from './myPostsContent/myPostsContent';
+import { useGetUserPosts } from './hooks/useGetUserPosts';
+import { Button } from '@/components/ui/button';
+import { Input } from '@/components/ui/input';
+import { useSearchUserPosts } from './hooks/useSearchUserPosts';
+import { Skeleton } from '@/components/ui/skeleton';
+// import Pagination from '@/components/Pagination';
+
+const PostSkeleton = () => (
+  <div className="mx-16 my-4 border-b border-gray-200 p-4">
+    <div className="flex items-start gap-4">
+      <Skeleton className="h-5 w-14" />
+      <Skeleton className="h-5 w-4/5" />
+      <div className="flex-col space-y-2">
+        <Skeleton className="h-5 w-10" />
+        <Skeleton className="h-4 w-10" />
+      </div>
+    </div>
+  </div>
+);
+
+const PostsLoadingSkeleton = () => {
+  return (
+    <>
+      {[...Array(6)].map((_, index) => (
+        <PostSkeleton key={index} />
+      ))}
+    </>
+  );
+};
 
 function MyPostsPage() {
+  const [page, setPage] = useState(0);
+  const [take] = useState(6);
   const [searchText, setSearchText] = useState('');
+  const [query, setQuery] = useState('');
+
+  const userPostsData = useGetUserPosts(page, take);
+  const searchData = useSearchUserPosts(page, take, query);
+
+  const data = query ? searchData.data : userPostsData.data;
+  const isLoading = query ? searchData.isLoading : userPostsData.isLoading;
+  const error = query ? searchData.error : userPostsData.error;
+
+  const totalPages = data?.pageInfo?.totalPages ?? 1;
 
   const onClickSearch = () => {
     console.log(searchText);
+    setPage(0);
+    setQuery(searchText);
   };
+
+  const handlePageClick = (pageNumber: number) => {
+    setPage(pageNumber);
+  };
+
+  const getPaginationRange = () => {
+    const totalPagesToShow = 6;
+    const startPage = Math.max(0, page - Math.floor(totalPagesToShow / 2));
+    const endPage = Math.min(totalPages, startPage + totalPagesToShow);
+    return Array.from({ length: endPage - startPage }, (_, i) => startPage + i);
+  };
+
+  if (isLoading) {
+    return (
+      <div className="flex flex-col">
+        <UserContainer />
+        <div>
+          <div className="mx-16 flex items-center border-b-[1px] border-solid border-gray-500 pb-1">
+            <span className="pl-2">작성 글</span>
+            <Skeleton className="ml-2 h-4 w-5" />
+          </div>
+          <PostsLoadingSkeleton />
+        </div>
+      </div>
+    );
+  }
+
+  if (error || !data) {
+    console.log('error : ', error);
+    return <div className="p-20">오류가 발생하였습니다.</div>;
+  }
 
   return (
     <div className="flex flex-col">
@@ -15,25 +89,53 @@ function MyPostsPage() {
       <div>
         <div className="mx-16 flex items-center border-b-[1px] border-solid border-gray-500 pb-1">
           <span className="pl-2">작성 글</span>
-          <span className="ml-2 text-center font-semibold">45</span>
+          <span className="ml-2 text-center font-semibold">{data.pageInfo.totalElements}</span>
         </div>
-        <MyPostsContent boardCode="건의게시판" postId={12345} />
-        <MyPostsContent boardCode="건의게시판" postId={13213} />
-        <MyPostsContent boardCode="건의게시판" postId={125} />
-        <MyPostsContent boardCode="건의게시판" postId={54321} />
-        <MyPostsContent boardCode="건의게시판" postId={222} />
-        <MyPostsContent boardCode="건의게시판" postId={5658} />
+        {data.postListResDto.map((post) => (
+          <MyPostsContent key={post.postId} data={post} />
+        ))}
       </div>
-      <div className="my-16 flex justify-center gap-3">
-        <input
+      <div className="mb-6 mt-14 flex items-center justify-center text-sm">
+        <button
+          onClick={() => setPage((prev) => Math.max(prev - 1, 0))}
+          disabled={page === 0}
+          className="border-none px-3 py-1 text-gray-700 disabled:opacity-30"
+        >
+          &lt;
+        </button>
+
+        {getPaginationRange().map((pageNumber) => (
+          <button
+            key={pageNumber}
+            onClick={() => handlePageClick(pageNumber)}
+            className={`rounded-sm border-none px-3 py-1 text-gray-700 ${page === pageNumber ? 'bg-gray-200' : 'bg-white'}`}
+          >
+            {pageNumber + 1}
+          </button>
+        ))}
+
+        <button
+          onClick={() => setPage((prev) => (prev + 1 < totalPages ? prev + 1 : prev))}
+          disabled={page + 1 >= totalPages}
+          className="border-none px-3 py-1 text-gray-700 disabled:opacity-30"
+        >
+          &gt;
+        </button>
+      </div>
+      {/* <Pagination totalPages={totalPages} currentPage={page} onPageChange={handlePageClick} /> */}
+      <div className="mb-16 flex justify-center gap-3">
+        <Input
           type="text"
-          onChange={(e) => setSearchText(e.target.value)}
-          className="w-[40%] rounded-md border border-gray-300 p-3 text-[13px]"
+          onChange={(e: { target: { value: string } }) => setSearchText(e.target.value)}
+          className="w-56 rounded-md border border-gray-300 p-3 text-xs md:w-80"
           placeholder="원하시는 키워드를 입력하세요"
         />
-        <button onClick={onClickSearch} className="rounded-md bg-[#2F4BF7] px-7 text-[13px] text-white">
+        <Button
+          onClick={onClickSearch}
+          className="h-12 rounded-md bg-[#2F4BF7] px-7 text-[13px] text-white xs:px-4 sm:px-5"
+        >
           검색
-        </button>
+        </Button>
       </div>
     </div>
   );
