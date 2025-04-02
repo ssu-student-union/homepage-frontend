cebddf001edf43707da70233b185d978c0cb20a0
jongse7 <mmssjj72@naver.com>
2025-04-02 22:25:37 +0900

    fix: snap 관련 스타일 제거 #489

--- src/pages/main/page.tsx
+++ src/pages/main/page.tsx
@@ -24,41 +24,39 @@ export function MainPage() {
 
   return (
     <>
-      <main className="h-screen snap-y snap-mandatory overflow-y-scroll">
-        {isLoading ? (
-          <div className="flex items-center justify-center">
-            <ServiceNoticeTab.Skeleton />
-          </div>
-        ) : isError ? (
-          <div className="flex items-center justify-center">
-            <p>에러가 발생했습니다. 잠시 후 다시 시도해주세요.</p>
-          </div>
-        ) : firstNotice ? (
-          <ServiceNoticeTab
-            isEmergency={firstNotice.status === '긴급공지'}
-            title={firstNotice?.title}
-            postId={firstNotice?.postId}
-          />
-        ) : null}
+      {isLoading ? (
+        <div className="flex items-center justify-center">
+          <ServiceNoticeTab.Skeleton />
+        </div>
+      ) : isError ? (
+        <div className="flex items-center justify-center">
+          <p>에러가 발생했습니다. 잠시 후 다시 시도해주세요.</p>
+        </div>
+      ) : firstNotice ? (
+        <ServiceNoticeTab
+          isEmergency={firstNotice.status === '긴급공지'}
+          title={firstNotice?.title}
+          postId={firstNotice?.postId}
+        />
+      ) : null}
 
-        <MainCarousel className="h-screen snap-start" id={MAIN_PENDING} />
-        <MainScheduleSection className="snap-ceneter" id={MAIN_PENDING} />
-        <CounselBtn />
+      <MainCarousel className="h-screen" id={MAIN_PENDING} />
+      <MainScheduleSection id={MAIN_PENDING} />
+      <CounselBtn />
 
-        <div className="snap-start xs:px-[15px] sm:px-[15px] md:px-[3.125rem] lg:px-[12.5rem] xl:px-[12.5rem] xxl:px-[12.5rem]">
-          <Spacing size={86} direction="vertical" />
-          <QnaSection />
-          <Spacing size={width >= 720 ? 142 : 82} direction="vertical" />
-          <NoticeSection />
-          <Spacing size={width >= 720 ? 142 : 82} direction="vertical" />
-          {/* 기존 공약사업 기능 임시 제거 */}
-          {/* <div className="인기청원 분실물 w-full flex-col space-y-5 overflow-hidden">
+      <div className="snap-start xs:px-[15px] sm:px-[15px] md:px-[3.125rem] lg:px-[12.5rem] xl:px-[12.5rem] xxl:px-[12.5rem]">
+        <Spacing size={86} direction="vertical" />
+        <QnaSection />
+        <Spacing size={width >= 720 ? 142 : 82} direction="vertical" />
+        <NoticeSection />
+        <Spacing size={width >= 720 ? 142 : 82} direction="vertical" />
+        {/* 기존 공약사업 기능 임시 제거 */}
+        {/* <div className="인기청원 분실물 w-full flex-col space-y-5 overflow-hidden">
             <PetitionSection />
             <LostArticleSection />
           </div> */}
-          <CampusMapSection />
-        </div>
-      </main>
+        <CampusMapSection />
+      </div>
       <Spacing size={202} direction="vertical" />
     </>
   );
