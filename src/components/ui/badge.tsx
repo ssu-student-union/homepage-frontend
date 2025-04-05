e8ed2e90b0b98d46c15a769fd8afc60232be092f
EATSTEAK <me@eatsteak.dev>
2025-04-06 00:26:18 +0900

    feat: update badge variants

--- src/components/ui/badge.tsx
+++ src/components/ui/badge.tsx
@@ -4,29 +4,35 @@ import { cva, type VariantProps } from 'class-variance-authority';
 import { cn } from '@/libs/utils';
 
 const badgeVariants = cva(
-  'inline-flex items-center rounded-full border px-2.5 py-0.5 text-xs font-semibold transition-colors focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2',
+  'inline-flex items-center rounded-full border font-semibold transition-colors focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2',
   {
     variants: {
       variant: {
         default: 'border-transparent bg-primary text-primary-foreground hover:bg-primary/80',
         secondary: 'border-transparent bg-secondary text-secondary-foreground hover:bg-secondary/80',
-        destructive: 'border-transparent bg-destructive text-destructive-foreground hover:bg-destructive/80',
+        destructive: 'border-transparent bg-red-700 text-destructive-foreground hover:bg-red-700/80',
         outline: 'text-foreground',
         'emergency-old':
           'bg-red-500 rounded-[21px] w-[54px] h-[23px] flex justify-center text-white border-none absolute top-[-10px] right-[-10px]',
-        'new-old': 'bg-primary rounded-[21px] w-[54px] h-[23px] flex justify-center text-white border-none absolute top-[-10px] right-[-10px]',
+        'new-old':
+          'bg-primary rounded-[21px] w-[54px] h-[23px] flex justify-center text-white border-none absolute top-[-10px] right-[-10px]',
+      },
+      size: {
+        default: 'px-2.5 py-0.5 text-xs',
+        sm: 'px-2 py-0.5 text-[10px] leading-[12px]',
       },
     },
     defaultVariants: {
       variant: 'default',
+      size: 'default',
     },
   }
 );
 
 export interface BadgeProps extends React.HTMLAttributes<HTMLDivElement>, VariantProps<typeof badgeVariants> {}
 
-function Badge({ className, variant, ...props }: BadgeProps) {
-  return <div className={cn(badgeVariants({ variant }), className)} {...props} />;
+function Badge({ className, variant, size, ...props }: BadgeProps) {
+  return <div className={cn(badgeVariants({ variant, size }), className)} {...props} />;
 }
 
 export { Badge };
