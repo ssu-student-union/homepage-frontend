9e9745e7f9c6220d235f46241ac026a38ca491c6
EATSTEAK <me@eatsteak.dev>
2025-04-05 21:15:29 +0900

    design: update `Category` style

--- src/components/Category/index.tsx
+++ src/components/Category/index.tsx
@@ -10,10 +10,10 @@ export function Category({ isActive = false, children, className = '', ...props 
   return (
     <button
       className={cn(
-        `flex h-[31px] min-w-fit items-center justify-center rounded-[32px] border border-gray-800 px-[16px] py-[8px] text-[0.875rem] font-bold transition-colors duration-100 md:h-[38px] md:text-[1.125rem] ${
+        `flex h-[38px] min-w-fit items-center justify-center rounded-[32px] border px-[16px] py-[8px] text-[1.125rem] transition-colors duration-100 xs:h-[31px] xs:text-[0.875rem] sm:h-[31px] sm:text-[0.875rem] ${
           isActive
-            ? 'bg border-none bg-primary text-white'
-            : 'border border-black bg-white text-black hover:bg-gray-100'
+            ? 'bg border-none bg-primary font-bold text-white'
+            : 'border border-neutral-200 bg-white font-medium text-neutral-600 hover:bg-neutral-100'
         } `,
         className
       )}
