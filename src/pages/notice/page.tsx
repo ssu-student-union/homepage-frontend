222d32437202a4ed369320976797c3eac93bb8c9
EATSTEAK <me@eatsteak.dev>
2025-04-09 22:13:19 +0900

    design(main): redesign `NoticeSection`

--- src/pages/notice/page.tsx
+++ src/pages/notice/page.tsx
@@ -13,58 +13,11 @@ import { cn } from '@/libs/utils';
 import { useSearchNoticePosts } from '@/pages/notice/queries';
 import { Pencil } from 'lucide-react';
 import { useMemo } from 'react';
-import { Link, To, useSearchParams } from 'react-router';
+import { Link, useSearchParams } from 'react-router';
 import { Skeleton } from '@/components/ui/skeleton';
 import { useTranslation } from 'react-i18next';
-import { TFunction } from 'i18next';
-
-const buildCentralSubCategories: (t: TFunction) => { id: string; name: string; to: To }[] = (t) => [
-  { id: '전체', name: t('board-selector.전체'), to: { search: '?category=중앙' } },
-  { id: '총학생회', name: t('board-selector.총학생회'), to: { search: '?category=중앙&sub=총학생회' } },
-  {
-    id: '중앙운영위원회',
-    name: t('board-selector.중앙운영위원회'),
-    to: { search: '?category=중앙&sub=중앙운영위원회' },
-  },
-  {
-    id: '중앙감사위원회',
-    name: t('board-selector.중앙감사위원회'),
-    to: { search: '?category=중앙&sub=중앙감사위원회' },
-  },
-  {
-    id: '중앙선거관리위원회',
-    name: t('board-selector.중앙선거관리위원회'),
-    to: { search: '?category=중앙&sub=중앙선거관리위원회' },
-  },
-  { id: '동아리연합회', name: t('board-selector.동아리연합회'), to: { search: '?category=중앙&sub=동아리연합회' } },
-];
-
-const buildCollageSubCategories: (t: TFunction) => { id: string; name: string; to: To }[] = (t) => [
-  { id: '전체', name: t('board-selector.전체'), to: { search: '?category=단과대' } },
-  { id: '경영대학', name: t('board-selector.경영대학'), to: { search: '?category=단과대&sub=경영대학' } },
-  { id: '경제통상대학', name: t('board-selector.경제통상대학'), to: { search: '?category=단과대&sub=경제통상대학' } },
-  { id: '공과대학', name: t('board-selector.공과대학'), to: { search: '?category=단과대&sub=공과대학' } },
-  { id: '법과대학', name: t('board-selector.법과대학'), to: { search: '?category=단과대&sub=법과대학' } },
-  { id: '사회과학대학', name: t('board-selector.사회과학대학'), to: { search: '?category=단과대&sub=사회과학대학' } },
-  { id: '인문대학', name: t('board-selector.인문대학'), to: { search: '?category=단과대&sub=인문대학' } },
-  { id: '자연과학대학', name: t('board-selector.자연과학대학'), to: { search: '?category=단과대&sub=자연과학대학' } },
-  { id: 'IT대학', name: t('board-selector.IT대학'), to: { search: '?category=단과대&sub=IT대학' } },
-  {
-    id: '융합특성화자유전공학부',
-    name: '융합특성화자유전공학부',
-    to: { search: '?category=단과대&sub=융합특성화자유전공학부' },
-  },
-];
-
-const isToday = (date: string) => {
-  const today = new Date();
-  const postDate = new Date(date);
-  return (
-    today.getFullYear() === postDate.getFullYear() &&
-    today.getMonth() === postDate.getMonth() &&
-    today.getDate() === postDate.getDate()
-  );
-};
+import { buildCentralSubCategories, buildCollageSubCategories } from '@/pages/notice/const';
+import { isToday } from '@/pages/notice/utils';
 
 const TodaySubtitle = ({ count }: { count: number }) => {
   return (
@@ -94,8 +47,8 @@ export function NoticePage() {
     groupCode: category === '중앙' ? '중앙기구' : '단과대학생회',
     memberCode: subCategory === '전체' ? '' : subCategory,
   });
-
   const today = useMemo(() => data?.postListResDto?.filter((post) => isToday(post.date))?.length ?? 0, [data]);
+
   const totalPages = useMemo(() => data?.pageInfo.totalPages ?? 0, [data]);
   const authorities = useMemo(() => data?.allowedAuthorities ?? [], [data]);
   const writable = useMemo(() => authorities.includes('WRITE'), [authorities]);
