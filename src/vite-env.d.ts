42d72b592de99740ca6fe5f3fb6886a39425e490
EATSTEAK <me@eatsteak.dev>
2025-04-09 22:11:48 +0900

    feat: import tailwind breakpoints as module

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,2 +1,5 @@
 /// <reference types="vite/client" />
 /// <reference types="vite-plugin-svgr/client" />
+declare module 'tailwind:breakpoints' {
+  export const breakpoints: Record<string, string>;
+}
