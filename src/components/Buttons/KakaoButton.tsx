10e9329799a651893cc142266b3c9cdddbceefb5
Patrick-Gong <owlcountry01@gmail.com>
2025-05-04 15:22:07 +0900

    refactor: 이미지로 된 kakaoButton에 Link 태그 추가하여 다이렉팅 구조 수정

--- src/components/Buttons/KakaoButton.tsx
+++ src/components/Buttons/KakaoButton.tsx
@@ -1,13 +1,25 @@
 import { useTranslation } from 'react-i18next';
+import { Link } from 'react-router';
+import { baseUrl } from '@/pages/kakao/containers/const/data';
+const rest_api_key = import.meta.env.VITE_REST_API_KEY;
+const redirect_uri = import.meta.env.VITE_REDIRECT_URI;
+// 카카오 로그인 리다이렉트 수정되면 경로 마꿔야함 - 학생회 이름 안 바뀜
+const TAG = ['ussum_001', 'ussum_002', 'ussum_003'];
+
+const KAKAO_AUTH_URL = `https://kauth.kakao.com/oauth/authorize?response_type=code&client_id=${rest_api_key}&redirect_uri=${baseUrl}${redirect_uri}&service_terms=${TAG}`;
 
 export function KakaoButton() {
   const { i18n } = useTranslation();
 
   const isTrans = i18n.language === 'en';
 
-  return isTrans ? (
-    <img src="/image/kakao_login_large_narrow_en.svg" alt="Login with Kakao" className="cursor-pointer" />
-  ) : (
-    <img src="/image/kakao_login_large_narrow.svg" alt="카카오 로그인" className="cursor-pointer" />
+  return (
+    <Link to={KAKAO_AUTH_URL}>
+      {isTrans ? (
+        <img src="/image/kakao_login_large_narrow_en.svg" alt="Login with Kakao" className="cursor-pointer" />
+      ) : (
+        <img src="/image/kakao_login_large_narrow.svg" alt="카카오 로그인" className="cursor-pointer" />
+      )}
+    </Link>
   );
 }
--- src/pages/kakao/containers/RegisterButtonSection.tsx
+++ src/pages/kakao/containers/RegisterButtonSection.tsx
@@ -1,17 +1,7 @@
 import { Link } from 'react-router';
 import { KakaoButton } from '@/components/Buttons/KakaoButton';
-import { baseUrl } from '@/pages/kakao/containers/const/data';
-import { useTranslation } from 'react-i18next';
 import { STUDENT_COUNCIL_NAME } from '@/const/studentCouncil';
-const rest_api_key = import.meta.env.VITE_REST_API_KEY;
-const redirect_uri = import.meta.env.VITE_REDIRECT_URI;
-const TAG = ['ussum_001', 'ussum_002', 'ussum_003'];
-
-const KAKAO_AUTH_URL = `https://kauth.kakao.com/oauth/authorize?response_type=code&client_id=${rest_api_key}&redirect_uri=${baseUrl}${redirect_uri}&service_terms=${TAG}`;
-
-const handleLogin = () => {
-  window.location.href = KAKAO_AUTH_URL;
-};
+import { useTranslation } from 'react-i18next';
 
 export function RegisterButtonSection() {
   const { t } = useTranslation();
@@ -20,9 +10,7 @@ export function RegisterButtonSection() {
       <div className="flex flex-col items-center text-center">
         <h1 className="mb-[-10px] text-xs font-normal">{t('onboarding.제65대 총학생회')}</h1>
         <h1 className="text-[56px] font-bold">{STUDENT_COUNCIL_NAME}</h1>
-        <div onClick={handleLogin}>
-          <KakaoButton />
-        </div>
+        <KakaoButton />
         <Link to={'/register/scouncil'}>
           <div className="mt-[20px] text-[12px] font-medium not-italic leading-[130%] text-[#828282] underline">
             {t('onboarding.학생자치기구 로그인')}
