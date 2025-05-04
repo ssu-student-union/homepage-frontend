9506ab26cec2a842b8c02dd5d59622d59709b618
Patrick-Gong <owlcountry01@gmail.com>
2025-05-04 14:54:57 +0900

    refactor: Kakao Button.tsx Boolean 명시 해제 및 대체 텍스트 변경

--- src/components/Buttons/KakaoButton.tsx
+++ src/components/Buttons/KakaoButton.tsx
@@ -3,11 +3,11 @@ import { useTranslation } from 'react-i18next';
 export function KakaoButton() {
   const { i18n } = useTranslation();
 
-  const isTrans = Boolean(i18n.language === 'en');
+  const isTrans = i18n.language === 'en';
 
   return isTrans ? (
-    <img src="/image/kakao_login_large_narrow_en.svg" alt="Kakao Login Button(English)" className="cursor-pointer" />
+    <img src="/image/kakao_login_large_narrow_en.svg" alt="Login with Kakao" className="cursor-pointer" />
   ) : (
-    <img src="/image/kakao_login_large_narrow.svg" alt="Kakao Login Button(Korean)" className="cursor-pointer" />
+    <img src="/image/kakao_login_large_narrow.svg" alt="카카오 로그인" className="cursor-pointer" />
   );
 }
