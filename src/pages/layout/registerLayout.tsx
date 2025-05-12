80568f322bec4e9bfdbeaf9595fc261f1ca4712c
Patrick-Gong <154777446+Patrick-Gong@users.noreply.github.com>
2025-05-12 14:38:24 +0900

    fix: passu 리다이렉트 자치기구 로그인 기능 추가 (#533)
    ## 1️⃣ 작업 내용 Summary
    
    - resolved #532 
    Passu에서 넘어올 때 자치기구 로그인 성공 시 passu로 리다이렉트하는 부분을 각주 처리하였는데 이 부분 필요하여서 다시
    추가해놓았습니다.
    추가로 #526 로그인 성공 시 토큰 저장 로직 추가 pr이 누락되어서 그 부분도 추가하였습니다.
    
    ## 2️⃣ 추후 작업할 내용
    
    ## 3️⃣ 체크리스트
    
    - [x] `develop` 브랜치의 최신 코드를 `pull` 받았나요?

--- src/pages/layout/registerLayout.tsx
+++ src/pages/layout/registerLayout.tsx
@@ -1,32 +1,11 @@
-import { useEffect } from 'react';
-import { Outlet, useNavigate } from 'react-router';
+import { Outlet } from 'react-router';
 import { Header } from '@/containers/common/Header/Header';
 import { State } from '@/containers/common/Header/const/state';
-import { LoginState } from '@/atoms/atom';
-import { useAtom } from 'jotai';
 
 export function RegisterLayout() {
-  const [loginState, setLoginState] = useAtom(LoginState);
-  const navigate = useNavigate();
-
-  useEffect(() => {
-    const token = localStorage.getItem('accessToken');
-    if (token) {
-      setLoginState(true);
-    } else {
-      setLoginState(false);
-    }
-  }, [setLoginState]);
-
-  const handleLogout = () => {
-    localStorage.clear();
-    setLoginState(false);
-    navigate('/');
-  };
-
   return (
     <div className="flex min-h-screen flex-col">
-      <Header state={loginState ? State.Login : State.Logout} onLogout={handleLogout} />
+      <Header state={State.Onboarding} />
       <main className="grow">
         <Outlet />
       </main>
