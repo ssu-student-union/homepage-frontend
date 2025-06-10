7d6ef4c098a8c2d09052d9b22acfbd6d6a291183
문세종(RAIL) <78732904+jongse7@users.noreply.github.com>
2025-06-10 14:43:34 +0900

    chore: 단과대 공지 카테고리 변경 - 융합특성화자율전공학부 -> 자유전공학부 (#565)
    ## 1️⃣ 작업 내용 Summary
    
    - resolved #564 
    
    단과대 공지사항 카테고리에서 융특을 자유전공학부로 변경했습니다.
    
    ## 2️⃣ 추후 작업할 내용
    
    ## 3️⃣ 체크리스트
    
    - [x] `develop` 브랜치의 최신 코드를 `pull` 받았나요?

--- src/pages/notice/const.ts
+++ src/pages/notice/const.ts
@@ -27,7 +27,7 @@ export const subName2: string[] = [
   '인문대학',
   '자연과학대학',
   'IT대학',
-  '융합특성화자유전공학부',
+  '자유전공학부',
 ];
 
 export const buildCentralSubCategories: (t: TFunction) => { id: string; name: string; to: To }[] = (t) => [
@@ -62,8 +62,8 @@ export const buildCollageSubCategories: (t: TFunction) => { id: string; name: st
   { id: '자연과학대학', name: t('board-selector.자연과학대학'), to: { search: '?category=단과대&sub=자연과학대학' } },
   { id: 'IT대학', name: t('board-selector.IT대학'), to: { search: '?category=단과대&sub=IT대학' } },
   {
-    id: '융합특성화자유전공학부',
-    name: '융합특성화자유전공학부',
-    to: { search: '?category=단과대&sub=융합특성화자유전공학부' },
+    id: '자유전공학부',
+    name: t('board-selector.자유전공학부'),
+    to: { search: '?category=단과대&sub=자유전공학부' },
   },
 ];
