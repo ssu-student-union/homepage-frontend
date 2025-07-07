bc32b27210776e2557c6a2faa564d4bd10b4f004
ParkSumin <fhsjdvs@gmail.com>
2025-07-08 01:11:28 +0900

    refactor: 분실물 게시판 delete

--- src/pages/notice/[id]/utils/locationHandler.ts
+++ src/pages/notice/[id]/utils/locationHandler.ts
@@ -1,3 +1,5 @@
+import { LostArticlePost } from '@/pages/lost-article/schema';
+import { ServiceNoticePost } from '@/pages/mypage/service-notice/schema';
 import { NoticePost } from '@/pages/notice/schema';
 import { NavigateFunction } from 'react-router';
 
@@ -8,10 +10,22 @@ export interface dataType {
   };
 }
 
+export interface LostArticledataType {
+  data: {
+    postId: number;
+    postDetail?: LostArticlePost;
+  };
+}
+
+
 export const handleLocation = ({ data }: dataType, navigate: NavigateFunction) => {
   navigate(`/notice/${data.postId}/patch`, { state: { data } });
 };
 
 export const serviceNoticeHandleLocation = ({ data }: dataType, navigate: NavigateFunction) => {
   navigate(`/service-notice/${data.postId}/patch`, { state: { data } });
+};
+
+export const lostArticleHandleLocation = ({ data }: LostArticledataType, navigate: NavigateFunction) => {
+  navigate(`/lost-article/${data.postId}/patch`, { state: { data } });
 };
