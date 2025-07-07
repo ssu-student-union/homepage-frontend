36a6715251dfdbfa82a65654c22a9d7f613cfaf6
ParkSumin <fhsjdvs@gmail.com>
2025-07-08 01:19:15 +0900

    fix: lint error

--- src/pages/notice/[id]/utils/locationHandler.ts
+++ src/pages/notice/[id]/utils/locationHandler.ts
@@ -1,5 +1,3 @@
-import { LostArticlePost } from '@/pages/lost-article/schema';
-import { ServiceNoticePost } from '@/pages/mypage/service-notice/schema';
 import { NoticePost } from '@/pages/notice/schema';
 import { NavigateFunction } from 'react-router';
 
@@ -10,22 +8,10 @@ export interface dataType {
   };
 }
 
-export interface LostArticledataType {
-  data: {
-    postId: number;
-    postDetail?: LostArticlePost;
-  };
-}
-
-
 export const handleLocation = ({ data }: dataType, navigate: NavigateFunction) => {
   navigate(`/notice/${data.postId}/patch`, { state: { data } });
 };
 
 export const serviceNoticeHandleLocation = ({ data }: dataType, navigate: NavigateFunction) => {
   navigate(`/service-notice/${data.postId}/patch`, { state: { data } });
-};
-
-export const lostArticleHandleLocation = ({ data }: LostArticledataType, navigate: NavigateFunction) => {
-  navigate(`/lost-article/${data.postId}/patch`, { state: { data } });
 };
