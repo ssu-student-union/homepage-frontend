d06e8a62b4c286e3da96e484c44143bd2995d068
Patrick-Gong <owlcountry01@gmail.com>
2025-07-08 17:46:12 +0900

    refactor: 목록 페이지 자료집 레이아웃으로 통일

--- src/pages/mypage/service-notice/page.tsx
+++ src/pages/mypage/service-notice/page.tsx
@@ -1,72 +1,63 @@
-import { HeadLayout } from '@/template/HeadLayout';
 import { ServiceNoticePostContent } from './component/ServiceNoticePostContent';
-import Pagination from '@/components/deprecated/Pagination';
-import { WriteButton } from '@/components/deprecated/Buttons/BoardActionButtons';
-import { useNavigate } from 'react-router';
-import { useContentWidth } from './hooks/useContetnWidth';
+import { Link } from 'react-router';
 import { cn } from '@/libs/utils';
 import { useServiceNoticeBoard } from './hooks/useServiceNoticeBoard';
 import dayjs from 'dayjs';
-
+import { BoardHeader } from '@/components/BoardHeader';
+import { Container } from '@/containers/new/Container';
+import { ArticleFooter } from '@/containers/new/ArticleFooter';
+import LinkPagination from '@/components/LinkPagination';
+import { Pencil } from 'lucide-react';
+import { buttonVariants } from '@/components/ui/button';
 export function ServiceNoticePage() {
-  const contentWidth = useContentWidth();
-
   const boardCode = '서비스공지사항';
-  const { idata, totalPages, currentPage, handlePageChange, isLoading } = useServiceNoticeBoard(boardCode);
+  const { idata, totalPages, currentPage, isLoading } = useServiceNoticeBoard(boardCode);
   const data = idata?.data.postListResDto;
-  const navigate = useNavigate();
-
-  const handleWriteBtnClick = () => {
-    navigate('/service-notice/edit');
-  };
-
-  const MobileWriteBtn = contentWidth === 316 ? 'justify-center' : 'justify-end';
 
   return (
     <>
-      <div className="mb-[310px]">
-        <div>
-          <HeadLayout
-            title="서비스 공지사항"
-            subtitle="홈페이지 개발자의 공지사항을 업로드합니다"
-            searchHidden={true}
-          />
-        </div>
-        {/* 로딩 상태에 따라 Skeleton 또는 실제 데이터를 표시 */}
-        <div className="flex flex-col items-center justify-center">
-          {isLoading ? (
-            <div className="mb-[300px] mt-[64px] flex w-full flex-col items-center">
-              <div className={cn(`flex w-full flex-col flex-wrap gap-[10px]`)}>
-                {Array.from({ length: 7 }).map((_, index) => (
-                  <ServiceNoticePostContent.Skeleton key={index} />
-                ))}
-              </div>
+      <BoardHeader
+        title="서비스 공지사항"
+        subtitle="홈페이지 개발자의 공지사항을 업로드합니다"
+        className="border-b-neutral-200 max-md:px-5 md:border-b"
+      />
+      <Container className="pt-0 max-md:px-0 md:pt-14">
+        {isLoading ? (
+          <div className={cn(`flex w-full flex-col flex-wrap gap-[10px]`)}>
+            {Array.from({ length: 7 }).map((_, index) => (
+              <ServiceNoticePostContent.Skeleton key={index} />
+            ))}
+          </div>
+        ) : (
+          data?.map((data) => (
+            <ServiceNoticePostContent
+              key={data.postId}
+              postId={data.postId.toString()}
+              title={data.title}
+              date={dayjs(data.date).format('YYYY-MM-DD')}
+              Emergency={data.status === '긴급공지'}
+            />
+          ))
+        )}
+      </Container>
+      <ArticleFooter className="mb-20">
+        <div className="flex flex-col gap-9">
+          <div className="grid grid-cols-3">
+            <div></div>
+            <div className="flex justify-center">
+              <LinkPagination totalPages={totalPages} maxDisplay={7} page={currentPage} />
             </div>
-          ) : (
-            <>
-              <div className="mb-[40px] mt-[64px] flex flex-col items-center justify-center">
-                {data?.map((data) => (
-                  <ServiceNoticePostContent
-                    key={data.postId}
-                    postId={data.postId.toString()}
-                    title={data.title}
-                    date={dayjs(data.date).format('YYYY-MM-DD')}
-                    Emergency={data.status === '긴급공지'}
-                  />
-                ))}
-              </div>
-              <div className={`mb-[40px] flex ${MobileWriteBtn}`} style={{ width: `${contentWidth}px` }}>
-                <div onClick={handleWriteBtnClick}>
-                  {idata?.data.allowedAuthorities?.includes('WRITE') ? (
-                    <WriteButton onClick={handleWriteBtnClick} />
-                  ) : null}
-                </div>
-              </div>
-              <Pagination totalPages={totalPages} currentPage={currentPage} onPageChange={handlePageChange} />
-            </>
-          )}
+            <div className="flex justify-end">
+              {idata?.data.allowedAuthorities?.includes('WRITE') && (
+                <Link className={cn(buttonVariants({ variant: 'outline' }), 'gap-2')} to="/data/edit">
+                  <Pencil className="size-4" />
+                  <p>글쓰기</p>
+                </Link>
+              )}
+            </div>
+          </div>
         </div>
-      </div>
+      </ArticleFooter>
     </>
   );
 }
