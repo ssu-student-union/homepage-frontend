62dd19216eddd892054537dbd4352fc02e7de28f
Patrick-Gong <owlcountry01@gmail.com>
2025-07-12 22:51:58 +0900

    refactor: 서비스 공지사항 fetch hook 교체

--- src/pages/mypage/service-notice/component/ServiceNoticePostContent.tsx
+++ src/pages/mypage/service-notice/component/ServiceNoticePostContent.tsx
@@ -1,11 +1,11 @@
 import { Badge } from '@/components/ui/badge';
 import { useContentWidth } from '../hooks/useContetnWidth';
-import { useNavigate } from 'react-router';
 import { cn } from '@/libs/utils';
 import { Skeleton } from '@/components/ui/skeleton';
 import { formatYYYYMMDD } from '@/utils/formatYYYYMMDD';
-
-interface ServiceNoticePostContentProps {
+import { LinkProps } from 'react-router';
+import { Link } from 'react-router';
+interface ServiceNoticePostContentProps extends LinkProps {
   postId?: string;
   title?: string;
   date?: string;
@@ -13,21 +13,23 @@ interface ServiceNoticePostContentProps {
   className?: string;
 }
 
-export function ServiceNoticePostContent({ postId, title, date, Emergency, className }: ServiceNoticePostContentProps) {
+export function ServiceNoticePostContent({
+  title,
+  date,
+  Emergency,
+  className,
+  ...props
+}: ServiceNoticePostContentProps) {
   const contentWidth = useContentWidth();
-  const navigate = useNavigate();
   const formattedDate = date ? formatYYYYMMDD(date) : '';
   const mobileText =
     contentWidth === 316
       ? 'text-[12px] font-[500]' // Style for width 316
       : ''; // Default style
 
-  const handleTitleClick = () => {
-    navigate(`/service-notice/${postId}`);
-  };
-
   return (
-    <div
+    <Link
+      {...props}
       className={cn('flex h-[64px] border-b-[1px] border-[#9CA3AF]', className)}
       style={{ width: `${contentWidth}px` }}
     >
@@ -38,14 +40,11 @@ export function ServiceNoticePostContent({ postId, title, date, Emergency, class
       ) : (
         <div className="h-[23px] w-[54px]"></div>
       )}
-      <div
-        className="flex w-full cursor-pointer items-center justify-between gap-[8px] font-medium"
-        onClick={handleTitleClick}
-      >
+      <div className="flex w-full cursor-pointer items-center justify-between gap-[8px] font-medium">
         <span className={`ml-[20px] ${mobileText} cursor-pointer`}>{title}</span>
         <div className={`${mobileText} min-w-[63px] text-[#6B7280] md:min-w-0`}>{formattedDate}</div>
       </div>
-    </div>
+    </Link>
   );
 }
 
