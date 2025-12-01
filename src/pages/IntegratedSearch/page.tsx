465b21aaaf2bd1d681d7239d1406149d215f5157
ParkSumin <fhsjdvs@gmail.com>
2025-12-01 22:05:23 +0900

    chore:

--- src/pages/IntegratedSearch/page.tsx
+++ src/pages/IntegratedSearch/page.tsx
@@ -151,7 +151,7 @@ export function IntegratedSearch() {
                   onClick={() => {
                     // 다음 페이지 로드 로직 (API 연결 시 구현)
                   }}
-                  className="mx-auto h-[30px] w-[87px] rounded-full px-4 py-2 text-[12px] md:mx-0 md:size-fit md:text-[1rem]"
+                  className="mx-auto h-[30px] w-[87px] rounded-full px-4 py-2 text-[12px] md:mx-0 md:size-fit md:text[1rem]"
                 >
                   더보기 ({currentPage}/{totalPages})
                 </Button>
