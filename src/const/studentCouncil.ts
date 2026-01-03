f1cdee9546bcbf7af09a5c922df30604857ce14f
Patrick-Gong <owlcountry01@gmail.com>
2026-01-03 14:52:29 +0900

    fix: 로그인 화면학생회 이름 및 로고 수정

--- src/const/studentCouncil.ts
+++ src/const/studentCouncil.ts
@@ -1,7 +1,7 @@
 // 현 총학생회 명
-const STUDENT_COUNCIL_NAME = 'S:SURE' as const;
+const STUDENT_COUNCIL_NAME = 'FOCUSS:U' as const;
 
 // 총학생회 메일
-const STUDENT_COUNCIL_EMAIL = 'ssu65th@gmail.com' as const;
+const STUDENT_COUNCIL_EMAIL = 'focussu.official@gmail.com' as const;
 
 export { STUDENT_COUNCIL_NAME, STUDENT_COUNCIL_EMAIL };
