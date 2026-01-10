98c81b4aafffe9b0bd93a0e6a2113b128cf10f1d
Patrick-Gong <owlcountry01@gmail.com>
2026-01-10 17:25:32 +0900

    fix: i18n 영어 서수 적용

--- src/containers/common/Footer.tsx
+++ src/containers/common/Footer.tsx
@@ -1,11 +1,13 @@
 import { EnvelopeSimple } from '@phosphor-icons/react';
-import { STUDENT_COUNCIL_EMAIL, STUDENT_COUNCIL_NAME } from '@/const/studentCouncil';
+import { STUDENT_COUNCIL_EMAIL, STUDENT_COUNCIL_NUMBER, STUDENT_COUNCIL_NAME } from '@/const/studentCouncil';
 
 export const Footer = () => {
   return (
     <footer className="flex flex-col gap-4 bg-primary px-8 py-5 text-primary-foreground md:gap-5 md:px-10 md:py-8">
       <div className="flex flex-col gap-1 text-center md:gap-2 md:text-start">
-        <p className="text-lg font-semibold md:text-2xl">제66대 숭실대학교 총학생회 {STUDENT_COUNCIL_NAME}</p>
+        <p className="text-lg font-semibold md:text-2xl">
+          제 {STUDENT_COUNCIL_NUMBER}대 숭실대학교 총학생회 {STUDENT_COUNCIL_NAME}
+        </p>
         <p className="text-sm md:text-lg">서울시 동작구 상도로 369 숭실대학교 학생회관 106호 총학생회실</p>
       </div>
       <div className="flex items-center justify-center gap-1.5 text-sm md:justify-start md:text-lg">
