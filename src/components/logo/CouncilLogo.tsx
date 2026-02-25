466f783d00a35564c1ff11db7dd977d1cc09b700
공윤호 <154777446+Patrick-Gong@users.noreply.github.com>
2026-02-25 21:51:45 +0900

    feat: 66대 소개 및 조직도 반영 (#620)
    ## 1️⃣ 작업 내용 Summary
    
    - resolved #619 
    
    총학생회 소개 페이지 66대 총학생회 소개 및 조직도로 변경 
    
    ## 2️⃣ 추후 작업할 내용
    
    ## 3️⃣ 체크리스트
    
    - [x] `develop` 브랜치의 최신 코드를 `pull` 받았나요?

--- src/components/logo/CouncilLogo.tsx
+++ src/components/logo/CouncilLogo.tsx
@@ -1,5 +1,5 @@
 import LogoSvg from '@/assets/image/logo/focussu_logo.svg?react';
-// import SkyBlueLogoSvg from '@/assets/image/logo/focussu_logo_skyblue.svg?react';
+import SkyBlueLogoSvg from '@/assets/image/logo/focussu_logo_skyblue.svg?react';
 import { cn } from '@/libs/utils';
 
 // 66대 학생회 FOCUSSU 로고입니다.
@@ -8,6 +8,7 @@ export default function CouncilLogo({ className }: React.SVGProps<SVGSVGElement>
 }
 
 // 66대 학생회 FOCUSSU 로고 하늘색 버전입니다.
-// CouncilLogo.Skyblue = ({ className }: React.SVGProps<SVGSVGElement>) => {
-//   return <SkyBlueLogoSvg className={cn(className)} />;
-// };
+
+CouncilLogo.Skyblue = ({ className }: React.SVGProps<SVGSVGElement>) => {
+  return <SkyBlueLogoSvg className={cn(className)} />;
+};
