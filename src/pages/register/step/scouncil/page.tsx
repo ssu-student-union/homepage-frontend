1e41e854b0dae5776bc09d67203d1cd9e21811fc
ParkSumin <fhsjdvs@gmail.com>
2026-03-07 16:04:45 +0900

    fix: Prettier

--- src/pages/register/step/scouncil/page.tsx
+++ src/pages/register/step/scouncil/page.tsx
@@ -42,7 +42,12 @@ export function GeneralLoginPage() {
     setIsButtonDisabled(!isValid);
   }, [isValid]);
 
-  const setDataInLocalStorage = (data: { groupCodeList: string[]; memberName: string; majorName: string | null; accessToken: string }) => {
+  const setDataInLocalStorage = (data: {
+    groupCodeList: string[];
+    memberName: string;
+    majorName: string | null;
+    accessToken: string;
+  }) => {
     localStorage.setItem('groupCodeList', JSON.stringify(data?.groupCodeList));
     localStorage.setItem('memberName', data?.memberName);
     if (data?.majorName) {
